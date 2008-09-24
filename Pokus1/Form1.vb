c07fef8baad4012603129cf4478fd7707eb26a7b
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-24 16:41:00 +0000

    pokus s VB

---
+++ Pokus1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+
+
+        PictureBox1.ImageLocation = OpenFileDialog1.FileName
+        PictureBox1.Image() = System.Drawing.Image.FromFile(PictureBox1.ImageLocation)
+        TextBox1.Text = PictureBox1.ImageLocation
+        Label4.Text = PictureBox1.Image.Size.ToString
+
+
+    End Sub
+
+End Class
