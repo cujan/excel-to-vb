11bc3c3432b3b10ce275111d9956d8c258e1cf59
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-26 07:25:42 +0000

    pridal som form dodavatelia a upravil menu+ databaza

--- trunk/lesnictvo/lesnictvo/lesnictvo/My Project/Application.Designer.vb
+++ trunk/lesnictvo/lesnictvo/lesnictvo/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.lesnictvo.Form1
+            Me.MainForm = Global.lesnictvo.MDIParent1
         End Sub
     End Class
 End Namespace
