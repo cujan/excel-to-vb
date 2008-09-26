ce8cc42bf1ef7be7f725e532956684191f878f45
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-26 10:09:14 +0000

    updat esuborov

--- lesnictvo/lesnictvo/lesnictvo/MDIParent1.vb
+++ lesnictvo/lesnictvo/lesnictvo/MDIParent1.vb
@@ -96,4 +96,9 @@ Public Class MDIParent1
     Private Sub VšeobecnéÚdajeToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles VšeobecnéÚdajeToolStripMenuItem.Click
         vseobecne_udaje.Show()
     End Sub
+
+    Private Sub MDIParent1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+    End Sub
 End Class
--- lesnictvo/lesnictvo/lesnictvo/vseobecne_udaje.vb
+++ lesnictvo/lesnictvo/lesnictvo/vseobecne_udaje.vb
@@ -14,7 +14,7 @@
         Me.Vseobecne_udajeTableAdapter.Fill(Me.Vseobecne_udajeDataSet1.vseobecne_udaje)
         'TODO: This line of code loads data into the 'Vseobecne_udajeDataSet1.vseobecne_udaje' table. You can move, or remove it, as needed.
         Me.Vseobecne_udajeTableAdapter.Fill(Me.Vseobecne_udajeDataSet1.vseobecne_udaje)
-
+        Me.MdiParent = MDIParent1
     End Sub
 
     Private Sub Vseobecne_udajeBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
@@ -29,5 +29,12 @@
         Me.Vseobecne_udajeBindingSource.EndEdit()
         Me.TableAdapterManager.UpdateAll(Me.Vseobecne_udajeDataSet1)
 
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Validate()
+        Me.Vseobecne_udajeBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Vseobecne_udajeDataSet1)
+        Me.Close()
     End Sub
 End Class
