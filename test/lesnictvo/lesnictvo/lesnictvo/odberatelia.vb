5ddd54852136853bea7ccc296d77ca9cd1ce1621
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-26 20:09:51 +0000

    skuska

--- test/lesnictvo/lesnictvo/lesnictvo/odberatelia.vb
+++ test/lesnictvo/lesnictvo/lesnictvo/odberatelia.vb
@@ -1,6 +1,8 @@
 ﻿Public Class odberatelia
 
     Private Sub odberatelia_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Database1DataSet1.vseobecne_udaje' table. You can move, or remove it, as needed.
+        Me.Vseobecne_udajeTableAdapter.Fill(Me.Database1DataSet1.vseobecne_udaje)
         Me.MdiParent = MDIParent1
     End Sub
 End Class
