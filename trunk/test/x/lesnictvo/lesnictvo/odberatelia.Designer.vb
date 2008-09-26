08281cb5edf6e604c5569381e92537b36a63e677
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-26 19:34:50 +0000

    len daco skusam

--- trunk/test/x/lesnictvo/lesnictvo/odberatelia.Designer.vb
+++ trunk/test/x/lesnictvo/lesnictvo/odberatelia.Designer.vb
@@ -22,18 +22,31 @@ Partial Class odberatelia
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(222, 132)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
         '
         'odberatelia
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(771, 481)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "odberatelia"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Odberatelia"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
