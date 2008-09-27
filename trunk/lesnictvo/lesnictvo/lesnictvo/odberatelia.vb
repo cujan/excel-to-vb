6e4bb547e45bcc0d11b823fd308b576ffcdb05dc
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-27 15:40:39 +0000

    odobral som form novy odberatel

--- trunk/lesnictvo/lesnictvo/lesnictvo/odberatelia.vb
+++ trunk/lesnictvo/lesnictvo/lesnictvo/odberatelia.vb
@@ -18,6 +18,6 @@
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        novy_odberatel.Show()
+
     End Sub
 End Class
