e1a13db6cae6f361335cdaf65984ed197f35f4fa
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-29 16:30:17 +0000

    pridanie dynamickeho vytvarania riadkov do dodaci_list

--- lesnictvo/lesnictvo/dodaci_list.Designer.vb
+++ lesnictvo/lesnictvo/dodaci_list.Designer.vb
@@ -25,17 +25,9 @@ Partial Class dodaci_list
         Me.components = New System.ComponentModel.Container
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dodaci_list))
         Me.text_odberatel = New System.Windows.Forms.Label
-        Me.vybrat_odberatela = New System.Windows.Forms.Button
         Me.text_drevina = New System.Windows.Forms.Label
-        Me.drevina = New System.Windows.Forms.Label
-        Me.vybrat_drevinu = New System.Windows.Forms.Button
         Me.text_cena = New System.Windows.Forms.Label
-        Me.cena = New System.Windows.Forms.Label
-        Me.vybrat_cenu = New System.Windows.Forms.Button
         Me.text_mnozstvo = New System.Windows.Forms.Label
-        Me.monozstvo = New System.Windows.Forms.TextBox
-        Me.text_suma_celkom = New System.Windows.Forms.Label
-        Me.suma_celkom = New System.Windows.Forms.Label
         Me.zobrazit_fakturu = New System.Windows.Forms.Button
         Me.zobrazit_ppd = New System.Windows.Forms.Button
         Me.ulozit_dodaci_list = New System.Windows.Forms.Button
@@ -46,19 +38,21 @@ Partial Class dodaci_list
         Me.OdberateliaTableAdapter = New lesnictvo.odberateliaDataSet1TableAdapters.odberateliaTableAdapter
         Me.TableAdapterManager = New lesnictvo.odberateliaDataSet1TableAdapters.TableAdapterManager
         Me.OdberateliaBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton
         Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton
         Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton
         Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator
         Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox
-        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel
         Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator
         Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton
         Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton
         Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator
-        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton
-        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton
         Me.OdberateliaBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton
         Me.NazovComboBox = New System.Windows.Forms.ComboBox
+        Me.pridaj_riadok = New System.Windows.Forms.Button
+        Me.odober_riadok = New System.Windows.Forms.Button
         CType(Me.OdberateliaDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.OdberateliaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.OdberateliaBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -74,107 +68,36 @@ Partial Class dodaci_list
         Me.text_odberatel.TabIndex = 0
         Me.text_odberatel.Text = "Odberateľ:"
         '
-        'vybrat_odberatela
-        '
-        Me.vybrat_odberatela.Location = New System.Drawing.Point(461, 37)
-        Me.vybrat_odberatela.Name = "vybrat_odberatela"
-        Me.vybrat_odberatela.Size = New System.Drawing.Size(134, 23)
-        Me.vybrat_odberatela.TabIndex = 1
-        Me.vybrat_odberatela.Text = "Vyberte odberateľa"
-        Me.vybrat_odberatela.UseVisualStyleBackColor = True
-        '
         'text_drevina
         '
         Me.text_drevina.AutoSize = True
-        Me.text_drevina.Location = New System.Drawing.Point(47, 71)
+        Me.text_drevina.Location = New System.Drawing.Point(54, 134)
         Me.text_drevina.Name = "text_drevina"
-        Me.text_drevina.Size = New System.Drawing.Size(65, 13)
+        Me.text_drevina.Size = New System.Drawing.Size(62, 13)
         Me.text_drevina.TabIndex = 3
-        Me.text_drevina.Text = "Typ dreviny:"
-        '
-        'drevina
-        '
-        Me.drevina.AutoSize = True
-        Me.drevina.Location = New System.Drawing.Point(118, 71)
-        Me.drevina.Name = "drevina"
-        Me.drevina.Size = New System.Drawing.Size(44, 13)
-        Me.drevina.TabIndex = 4
-        Me.drevina.Text = "prázdny"
-        '
-        'vybrat_drevinu
-        '
-        Me.vybrat_drevinu.Location = New System.Drawing.Point(461, 66)
-        Me.vybrat_drevinu.Name = "vybrat_drevinu"
-        Me.vybrat_drevinu.Size = New System.Drawing.Size(134, 23)
-        Me.vybrat_drevinu.TabIndex = 5
-        Me.vybrat_drevinu.Text = "Vyberte drevinu"
-        Me.vybrat_drevinu.UseVisualStyleBackColor = True
+        Me.text_drevina.Text = "Typ dreviny"
         '
         'text_cena
         '
         Me.text_cena.AutoSize = True
-        Me.text_cena.Location = New System.Drawing.Point(19, 98)
+        Me.text_cena.Location = New System.Drawing.Point(437, 134)
         Me.text_cena.Name = "text_cena"
-        Me.text_cena.Size = New System.Drawing.Size(93, 13)
+        Me.text_cena.Size = New System.Drawing.Size(90, 13)
         Me.text_cena.TabIndex = 6
-        Me.text_cena.Text = "Jednotková cena:"
-        '
-        'cena
-        '
-        Me.cena.AutoSize = True
-        Me.cena.Location = New System.Drawing.Point(119, 98)
-        Me.cena.Name = "cena"
-        Me.cena.Size = New System.Drawing.Size(44, 13)
-        Me.cena.TabIndex = 7
-        Me.cena.Text = "prázdny"
-        '
-        'vybrat_cenu
-        '
-        Me.vybrat_cenu.Location = New System.Drawing.Point(462, 95)
-        Me.vybrat_cenu.Name = "vybrat_cenu"
-        Me.vybrat_cenu.Size = New System.Drawing.Size(133, 23)
-        Me.vybrat_cenu.TabIndex = 8
-        Me.vybrat_cenu.Text = "Vyberte cenu"
-        Me.vybrat_cenu.UseVisualStyleBackColor = True
+        Me.text_cena.Text = "Jednotková cena"
         '
         'text_mnozstvo
         '
         Me.text_mnozstvo.AutoSize = True
-        Me.text_mnozstvo.Location = New System.Drawing.Point(294, 97)
+        Me.text_mnozstvo.Location = New System.Drawing.Point(590, 134)
         Me.text_mnozstvo.Name = "text_mnozstvo"
-        Me.text_mnozstvo.Size = New System.Drawing.Size(56, 13)
+        Me.text_mnozstvo.Size = New System.Drawing.Size(53, 13)
         Me.text_mnozstvo.TabIndex = 9
-        Me.text_mnozstvo.Text = "Množstvo:"
-        '
-        'monozstvo
-        '
-        Me.monozstvo.Location = New System.Drawing.Point(356, 94)
-        Me.monozstvo.Name = "monozstvo"
-        Me.monozstvo.Size = New System.Drawing.Size(50, 20)
-        Me.monozstvo.TabIndex = 10
-        Me.monozstvo.Text = "0"
-        '
-        'text_suma_celkom
-        '
-        Me.text_suma_celkom.AutoSize = True
-        Me.text_suma_celkom.Location = New System.Drawing.Point(38, 147)
-        Me.text_suma_celkom.Name = "text_suma_celkom"
-        Me.text_suma_celkom.Size = New System.Drawing.Size(74, 13)
-        Me.text_suma_celkom.TabIndex = 11
-        Me.text_suma_celkom.Text = "Suma celkom:"
-        '
-        'suma_celkom
-        '
-        Me.suma_celkom.AutoSize = True
-        Me.suma_celkom.Location = New System.Drawing.Point(119, 147)
-        Me.suma_celkom.Name = "suma_celkom"
-        Me.suma_celkom.Size = New System.Drawing.Size(44, 13)
-        Me.suma_celkom.TabIndex = 12
-        Me.suma_celkom.Text = "prázdny"
+        Me.text_mnozstvo.Text = "Množstvo"
         '
         'zobrazit_fakturu
         '
-        Me.zobrazit_fakturu.Location = New System.Drawing.Point(100, 193)
+        Me.zobrazit_fakturu.Location = New System.Drawing.Point(487, 18)
         Me.zobrazit_fakturu.Name = "zobrazit_fakturu"
         Me.zobrazit_fakturu.Size = New System.Drawing.Size(103, 23)
         Me.zobrazit_fakturu.TabIndex = 13
@@ -183,7 +106,7 @@ Partial Class dodaci_list
         '
         'zobrazit_ppd
         '
-        Me.zobrazit_ppd.Location = New System.Drawing.Point(218, 193)
+        Me.zobrazit_ppd.Location = New System.Drawing.Point(487, 47)
         Me.zobrazit_ppd.Name = "zobrazit_ppd"
         Me.zobrazit_ppd.Size = New System.Drawing.Size(142, 23)
         Me.zobrazit_ppd.TabIndex = 14
@@ -192,7 +115,7 @@ Partial Class dodaci_list
         '
         'ulozit_dodaci_list
         '
-        Me.ulozit_dodaci_list.Location = New System.Drawing.Point(374, 193)
+        Me.ulozit_dodaci_list.Location = New System.Drawing.Point(487, 76)
         Me.ulozit_dodaci_list.Name = "ulozit_dodaci_list"
         Me.ulozit_dodaci_list.Size = New System.Drawing.Size(105, 23)
         Me.ulozit_dodaci_list.TabIndex = 15
@@ -257,6 +180,31 @@ Partial Class dodaci_list
         Me.OdberateliaBindingNavigator.Text = "BindingNavigator1"
         Me.OdberateliaBindingNavigator.Visible = False
         '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(36, 22)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
         'BindingNavigatorMoveFirstItem
         '
         Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
@@ -289,17 +237,10 @@ Partial Class dodaci_list
         Me.BindingNavigatorPositionItem.Text = "0"
         Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
         '
-        'BindingNavigatorCountItem
-        '
-        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
-        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(36, 13)
-        Me.BindingNavigatorCountItem.Text = "of {0}"
-        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
-        '
         'BindingNavigatorSeparator1
         '
-        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 25)
         '
         'BindingNavigatorMoveNextItem
         '
@@ -307,7 +248,7 @@ Partial Class dodaci_list
         Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
         Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 22)
         Me.BindingNavigatorMoveNextItem.Text = "Move next"
         '
         'BindingNavigatorMoveLastItem
@@ -316,38 +257,20 @@ Partial Class dodaci_list
         Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
         Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
         Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 22)
         Me.BindingNavigatorMoveLastItem.Text = "Move last"
         '
         'BindingNavigatorSeparator2
         '
-        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
-        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
-        '
-        'BindingNavigatorAddNewItem
-        '
-        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
-        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
-        Me.BindingNavigatorAddNewItem.Text = "Add new"
-        '
-        'BindingNavigatorDeleteItem
-        '
-        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
-        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
-        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
-        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
-        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 20)
-        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 25)
         '
         'OdberateliaBindingNavigatorSaveItem
         '
         Me.OdberateliaBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
         Me.OdberateliaBindingNavigatorSaveItem.Image = CType(resources.GetObject("OdberateliaBindingNavigatorSaveItem.Image"), System.Drawing.Image)
         Me.OdberateliaBindingNavigatorSaveItem.Name = "OdberateliaBindingNavigatorSaveItem"
-        Me.OdberateliaBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.OdberateliaBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 22)
         Me.OdberateliaBindingNavigatorSaveItem.Text = "Save Data"
         '
         'NazovComboBox
@@ -356,17 +279,38 @@ Partial Class dodaci_list
         Me.NazovComboBox.DataSource = Me.OdberateliaBindingSource
         Me.NazovComboBox.DisplayMember = "nazov"
         Me.NazovComboBox.FormattingEnabled = True
-        Me.NazovComboBox.Location = New System.Drawing.Point(122, 42)
+        Me.NazovComboBox.Location = New System.Drawing.Point(122, 43)
         Me.NazovComboBox.Name = "NazovComboBox"
         Me.NazovComboBox.Size = New System.Drawing.Size(121, 21)
         Me.NazovComboBox.TabIndex = 20
         Me.NazovComboBox.ValueMember = "nazov"
         '
+        'pridaj_riadok
+        '
+        Me.pridaj_riadok.Location = New System.Drawing.Point(737, 134)
+        Me.pridaj_riadok.Name = "pridaj_riadok"
+        Me.pridaj_riadok.Size = New System.Drawing.Size(22, 22)
+        Me.pridaj_riadok.TabIndex = 21
+        Me.pridaj_riadok.Text = "+"
+        Me.pridaj_riadok.UseVisualStyleBackColor = True
+        '
+        'odober_riadok
+        '
+        Me.odober_riadok.Location = New System.Drawing.Point(765, 134)
+        Me.odober_riadok.Name = "odober_riadok"
+        Me.odober_riadok.Size = New System.Drawing.Size(22, 22)
+        Me.odober_riadok.TabIndex = 22
+        Me.odober_riadok.Text = "--"
+        Me.odober_riadok.UseVisualStyleBackColor = True
+        '
         'dodaci_list
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(620, 305)
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(802, 307)
+        Me.Controls.Add(Me.odober_riadok)
+        Me.Controls.Add(Me.pridaj_riadok)
         Me.Controls.Add(Me.NazovComboBox)
         Me.Controls.Add(Me.OdberateliaBindingNavigator)
         Me.Controls.Add(Me.cislo_d_listu)
@@ -374,17 +318,9 @@ Partial Class dodaci_list
         Me.Controls.Add(Me.ulozit_dodaci_list)
         Me.Controls.Add(Me.zobrazit_ppd)
         Me.Controls.Add(Me.zobrazit_fakturu)
-        Me.Controls.Add(Me.suma_celkom)
-        Me.Controls.Add(Me.text_suma_celkom)
-        Me.Controls.Add(Me.monozstvo)
         Me.Controls.Add(Me.text_mnozstvo)
-        Me.Controls.Add(Me.vybrat_cenu)
-        Me.Controls.Add(Me.cena)
         Me.Controls.Add(Me.text_cena)
-        Me.Controls.Add(Me.vybrat_drevinu)
-        Me.Controls.Add(Me.drevina)
         Me.Controls.Add(Me.text_drevina)
-        Me.Controls.Add(Me.vybrat_odberatela)
         Me.Controls.Add(Me.text_odberatel)
         Me.Name = "dodaci_list"
         Me.Text = "Dodací list"
@@ -396,19 +332,18 @@ Partial Class dodaci_list
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
+        Me.cena_textbox_array = New cenaTextboxArray(Me)
+        Me.rezivo_textbox_array = New rezivoTextboxArray(Me)
+        Me.cena_button_array = New cenaButtonArray(Me)
+        Me.rezivo_button_array = New rezivoButtonArray(Me)
+
+
+
     End Sub
     Friend WithEvents text_odberatel As System.Windows.Forms.Label
-    Friend WithEvents vybrat_odberatela As System.Windows.Forms.Button
     Friend WithEvents text_drevina As System.Windows.Forms.Label
-    Friend WithEvents drevina As System.Windows.Forms.Label
-    Friend WithEvents vybrat_drevinu As System.Windows.Forms.Button
     Friend WithEvents text_cena As System.Windows.Forms.Label
-    Friend WithEvents cena As System.Windows.Forms.Label
-    Friend WithEvents vybrat_cenu As System.Windows.Forms.Button
     Friend WithEvents text_mnozstvo As System.Windows.Forms.Label
-    Friend WithEvents monozstvo As System.Windows.Forms.TextBox
-    Friend WithEvents text_suma_celkom As System.Windows.Forms.Label
-    Friend WithEvents suma_celkom As System.Windows.Forms.Label
     Friend WithEvents zobrazit_fakturu As System.Windows.Forms.Button
     Friend WithEvents zobrazit_ppd As System.Windows.Forms.Button
     Friend WithEvents ulozit_dodaci_list As System.Windows.Forms.Button
@@ -432,4 +367,6 @@ Partial Class dodaci_list
     Friend WithEvents BindingNavigatorSeparator2 As System.Windows.Forms.ToolStripSeparator
     Friend WithEvents OdberateliaBindingNavigatorSaveItem As System.Windows.Forms.ToolStripButton
     Friend WithEvents NazovComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents pridaj_riadok As System.Windows.Forms.Button
+    Friend WithEvents odober_riadok As System.Windows.Forms.Button
 End Class
