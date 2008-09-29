6bff4916026d3321e4ea4abe1e4649700cd05bd8
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-29 08:01:11 +0000

    uprava dodacieho listu

--- lesnictvo/lesnictvo/dodaci_list.Designer.vb
+++ lesnictvo/lesnictvo/dodaci_list.Designer.vb
@@ -22,9 +22,10 @@ Partial Class dodaci_list
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dodaci_list))
         Me.text_odberatel = New System.Windows.Forms.Label
         Me.vybrat_odberatela = New System.Windows.Forms.Button
-        Me.odberatel = New System.Windows.Forms.Label
         Me.text_drevina = New System.Windows.Forms.Label
         Me.drevina = New System.Windows.Forms.Label
         Me.vybrat_drevinu = New System.Windows.Forms.Button
@@ -40,6 +41,28 @@ Partial Class dodaci_list
         Me.ulozit_dodaci_list = New System.Windows.Forms.Button
         Me.text_cislo_d_listu = New System.Windows.Forms.Label
         Me.cislo_d_listu = New System.Windows.Forms.Label
+        Me.OdberateliaDataSet1 = New lesnictvo.odberateliaDataSet1
+        Me.OdberateliaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.OdberateliaTableAdapter = New lesnictvo.odberateliaDataSet1TableAdapters.odberateliaTableAdapter
+        Me.TableAdapterManager = New lesnictvo.odberateliaDataSet1TableAdapters.TableAdapterManager
+        Me.OdberateliaBindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton
+        Me.OdberateliaBindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton
+        Me.NazovComboBox = New System.Windows.Forms.ComboBox
+        CType(Me.OdberateliaDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OdberateliaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OdberateliaBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.OdberateliaBindingNavigator.SuspendLayout()
         Me.SuspendLayout()
         '
         'text_odberatel
@@ -59,15 +82,6 @@ Partial Class dodaci_list
         Me.vybrat_odberatela.TabIndex = 1
         Me.vybrat_odberatela.Text = "Vyberte odberateľa"
         Me.vybrat_odberatela.UseVisualStyleBackColor = True
-        '
-        'odberatel
-        '
-        Me.odberatel.AutoSize = True
-        Me.odberatel.Location = New System.Drawing.Point(119, 42)
-        Me.odberatel.Name = "odberatel"
-        Me.odberatel.Size = New System.Drawing.Size(44, 13)
-        Me.odberatel.TabIndex = 2
-        Me.odberatel.Text = "prázdny"
         '
         'text_drevina
         '
@@ -203,11 +217,158 @@ Partial Class dodaci_list
         Me.cislo_d_listu.TabIndex = 17
         Me.cislo_d_listu.Text = "prázdny"
         '
+        'OdberateliaDataSet1
+        '
+        Me.OdberateliaDataSet1.DataSetName = "odberateliaDataSet1"
+        Me.OdberateliaDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'OdberateliaBindingSource
+        '
+        Me.OdberateliaBindingSource.DataMember = "odberatelia"
+        Me.OdberateliaBindingSource.DataSource = Me.OdberateliaDataSet1
+        '
+        'OdberateliaTableAdapter
+        '
+        Me.OdberateliaTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.odberateliaTableAdapter = Me.OdberateliaTableAdapter
+        Me.TableAdapterManager.UpdateOrder = lesnictvo.odberateliaDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'OdberateliaBindingNavigator
+        '
+        Me.OdberateliaBindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.OdberateliaBindingNavigator.BindingSource = Me.OdberateliaBindingSource
+        Me.OdberateliaBindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.OdberateliaBindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.OdberateliaBindingNavigator.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.OdberateliaBindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.OdberateliaBindingNavigatorSaveItem})
+        Me.OdberateliaBindingNavigator.Location = New System.Drawing.Point(0, 280)
+        Me.OdberateliaBindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.OdberateliaBindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.OdberateliaBindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.OdberateliaBindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.OdberateliaBindingNavigator.Name = "OdberateliaBindingNavigator"
+        Me.OdberateliaBindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.OdberateliaBindingNavigator.Size = New System.Drawing.Size(620, 25)
+        Me.OdberateliaBindingNavigator.TabIndex = 18
+        Me.OdberateliaBindingNavigator.Text = "BindingNavigator1"
+        Me.OdberateliaBindingNavigator.Visible = False
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 21)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(36, 13)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 6)
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(23, 20)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'OdberateliaBindingNavigatorSaveItem
+        '
+        Me.OdberateliaBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.OdberateliaBindingNavigatorSaveItem.Image = CType(resources.GetObject("OdberateliaBindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.OdberateliaBindingNavigatorSaveItem.Name = "OdberateliaBindingNavigatorSaveItem"
+        Me.OdberateliaBindingNavigatorSaveItem.Size = New System.Drawing.Size(23, 23)
+        Me.OdberateliaBindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'NazovComboBox
+        '
+        Me.NazovComboBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.OdberateliaBindingSource, "nazov", True))
+        Me.NazovComboBox.DataSource = Me.OdberateliaBindingSource
+        Me.NazovComboBox.DisplayMember = "nazov"
+        Me.NazovComboBox.FormattingEnabled = True
+        Me.NazovComboBox.Location = New System.Drawing.Point(122, 42)
+        Me.NazovComboBox.Name = "NazovComboBox"
+        Me.NazovComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.NazovComboBox.TabIndex = 20
+        Me.NazovComboBox.ValueMember = "nazov"
+        '
         'dodaci_list
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(620, 233)
+        Me.ClientSize = New System.Drawing.Size(620, 305)
+        Me.Controls.Add(Me.NazovComboBox)
+        Me.Controls.Add(Me.OdberateliaBindingNavigator)
         Me.Controls.Add(Me.cislo_d_listu)
         Me.Controls.Add(Me.text_cislo_d_listu)
         Me.Controls.Add(Me.ulozit_dodaci_list)
@@ -223,18 +384,21 @@ Partial Class dodaci_list
         Me.Controls.Add(Me.vybrat_drevinu)
         Me.Controls.Add(Me.drevina)
         Me.Controls.Add(Me.text_drevina)
-        Me.Controls.Add(Me.odberatel)
         Me.Controls.Add(Me.vybrat_odberatela)
         Me.Controls.Add(Me.text_odberatel)
         Me.Name = "dodaci_list"
         Me.Text = "Dodací list"
+        CType(Me.OdberateliaDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OdberateliaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OdberateliaBindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.OdberateliaBindingNavigator.ResumeLayout(False)
+        Me.OdberateliaBindingNavigator.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents text_odberatel As System.Windows.Forms.Label
     Friend WithEvents vybrat_odberatela As System.Windows.Forms.Button
-    Friend WithEvents odberatel As System.Windows.Forms.Label
     Friend WithEvents text_drevina As System.Windows.Forms.Label
     Friend WithEvents drevina As System.Windows.Forms.Label
     Friend WithEvents vybrat_drevinu As System.Windows.Forms.Button
@@ -250,4 +414,22 @@ Partial Class dodaci_list
     Friend WithEvents ulozit_dodaci_list As System.Windows.Forms.Button
     Friend WithEvents text_cislo_d_listu As System.Windows.Forms.Label
     Friend WithEvents cislo_d_listu As System.Windows.Forms.Label
+    Friend WithEvents OdberateliaDataSet1 As lesnictvo.odberateliaDataSet1
+    Friend WithEvents OdberateliaBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents OdberateliaTableAdapter As lesnictvo.odberateliaDataSet1TableAdapters.odberateliaTableAdapter
+    Friend WithEvents TableAdapterManager As lesnictvo.odberateliaDataSet1TableAdapters.TableAdapterManager
+    Friend WithEvents OdberateliaBindingNavigator As System.Windows.Forms.BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents OdberateliaBindingNavigatorSaveItem As System.Windows.Forms.ToolStripButton
+    Friend WithEvents NazovComboBox As System.Windows.Forms.ComboBox
 End Class
--- lesnictvo/lesnictvo/dodaci_list.vb
+++ lesnictvo/lesnictvo/dodaci_list.vb
@@ -1,5 +1,14 @@
 ﻿Public Class dodaci_list
     Private Sub dodaci_list_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'OdberateliaDataSet1.odberatelia' table. You can move, or remove it, as needed.
+        Me.OdberateliaTableAdapter.Fill(Me.OdberateliaDataSet1.odberatelia)
         Me.MdiParent = MDIParent1
+    End Sub
+
+    Private Sub OdberateliaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OdberateliaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.OdberateliaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.OdberateliaDataSet1)
+
     End Sub
 End Class
