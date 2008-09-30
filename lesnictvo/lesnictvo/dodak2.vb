2b0a23b13e35a57e77514ec8212be8613986956b
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-09-30 11:56:36 +0000

    oprava

--- lesnictvo/lesnictvo/dodak2.vb
+++ lesnictvo/lesnictvo/dodak2.vb
@@ -1,17 +1,11 @@
 ﻿Public Class dodak2
 
-    Private Sub Dodaci_listBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Dodaci_listBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.Dodaci_listBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Dodaci_listDataSet1)
+    Private Sub Dodaci_listBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        
 
     End Sub
 
     Private Sub dodak2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'Drevina_pohladDataSet1.dreviny' table. You can move, or remove it, as needed.
-        Me.DrevinyTableAdapter.Fill(Me.Drevina_pohladDataSet1.dreviny)
-        'TODO: This line of code loads data into the 'Dodaci_listDataSet1.dodaci_list' table. You can move, or remove it, as needed.
-        Me.Dodaci_listTableAdapter.Fill(Me.Dodaci_listDataSet1.dodaci_list)
-
+        
     End Sub
 End Class
