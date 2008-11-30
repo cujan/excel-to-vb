32368fb06d1aa25a7b21b0e993dffabed03f333d
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-11-30 19:14:40 +0000

    pridal som nove formulare

---
+++ evidencia_spz/evidencia_spz/karta_clena.vb
@@ -0,0 +1,19 @@
+﻿Public Class karta_clena
+
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.ClenoviaDataSet)
+
+    End Sub
+
+    Private Sub evidencia_clenov_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'ClenoviaDataSet.clenovia' table. You can move, or remove it, as needed.
+        Me.ClenoviaTableAdapter.Fill(Me.ClenoviaDataSet.clenovia)
+
+    End Sub
+
+    Private Sub MenoLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
