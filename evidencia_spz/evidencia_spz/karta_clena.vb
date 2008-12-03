a2560a7a4e6f5efb4cc90c4ef56d7a9270cbd242
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-12-03 20:36:56 +0000

    pridana nova databaza

--- evidencia_spz/evidencia_spz/karta_clena.vb
+++ evidencia_spz/evidencia_spz/karta_clena.vb
@@ -1,27 +1,3 @@
 ﻿Public Class karta_clena
 
-    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.ClenoviaDataSet)
-
-    End Sub
-
-    Private Sub karta_clena_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.Dock = DockStyle.Fill
-        Me.TopLevel = False
-        hlavna_aplikacia.hlavny_splitter.Panel2.Controls.Add(Me)
-        'TODO: This line of code loads data into the 'ClenoviaDataSet.clenovia' table. You can move, or remove it, as needed.
-        Me.ClenoviaTableAdapter.Fill(Me.ClenoviaDataSet.clenovia)
-
-    End Sub
-
-    Private Sub MenoLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
-
-    Private Sub zavri_karta_clena_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zavri_karta_clena.Click
-        Me.Hide()
-
-    End Sub
 End Class
