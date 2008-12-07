bd256bec4ab4ebb2af8f52b245f7bbab094956c6
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-12-07 22:54:59 +0000

    presunutie stareho projektu do tags

--- lesnictvo/lesnictvo/moznosti.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class moznosti
-    Private Sub moznosti_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.MdiParent = MDIParent1
-    End Sub
-End Class
--- lesnictvo/lesnictvo/vystavene_faktury.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class vystavene_faktury
-    Private Sub vystavene_faktury_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Me.MdiParent = MDIParent1
-    End Sub
-End Class
