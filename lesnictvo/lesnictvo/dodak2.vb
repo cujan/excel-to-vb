76851c50741c274456409d8224fdf01d4d4fdfb1
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-12-07 22:54:59 +0000

    presunutie stareho projektu do tags

---
+++ lesnictvo/lesnictvo/dodak2.vb
@@ -0,0 +1,43 @@
+﻿Public Class dodak2
+
+    Private Sub Dodaci_listBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        
+    End Sub
+
+    Private Sub dodak2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Pohlad_odberatelDataSet1.odberatelia' table. You can move, or remove it, as needed.
+        Me.OdberateliaTableAdapter.Fill(Me.Pohlad_odberatelDataSet1.odberatelia)
+        'TODO: This line of code loads data into the 'Pohlad_drevinaDataSet1.dreviny' table. You can move, or remove it, as needed.
+        Me.DrevinyTableAdapter.Fill(Me.Pohlad_drevinaDataSet1.dreviny)
+        'TODO: This line of code loads data into the 'Dodaci_listDataSet1.dodaci_list_detaily' table. You can move, or remove it, as needed.
+        Me.Dodaci_list_detailyTableAdapter.Fill(Me.Dodaci_listDataSet1.dodaci_list_detaily)
+        'TODO: This line of code loads data into the 'Dodaci_listDataSet1.dodaci_list' table. You can move, or remove it, as needed.
+        Me.Dodaci_listTableAdapter.Fill(Me.Dodaci_listDataSet1.dodaci_list)
+
+
+    End Sub
+
+
+    Private Sub Dodaci_listBindingNavigator_RefreshItems(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    
+
+    Private Sub Dodaci_listBindingNavigator_RefreshItems_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Dodaci_listBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Dodaci_listBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.Dodaci_listBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Dodaci_listDataSet1)
+
+        Me.Dodaci_list_detailyBindingSource.EndEdit()
+        Me.Dodaci_list_detailyTableAdapter.Update(Me.Dodaci_listDataSet1.dodaci_list_detaily)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        dodaci_list_rezivo_detaily.Show()
+    End Sub
+End Class
---
+++ lesnictvo/lesnictvo/jednotkaDTextboxArray.vb
@@ -0,0 +1,49 @@
+﻿Public Class jednotkaDTextboxArray
+    Inherits System.Collections.CollectionBase
+    Private ReadOnly HostForm As System.Windows.Forms.Form
+
+    Public Function AddNewTextbox() As System.Windows.Forms.TextBox
+        ' Create a new instance of the Button class.
+        Dim aTextbox As New System.Windows.Forms.TextBox()
+
+        ' Add the button to the collection's internal list.
+        Me.List.Add(aTextbox)
+        ' Add the button to the controls collection of the form 
+        ' referenced by the HostForm field.
+        HostForm.Controls.Add(aTextbox)
+        ' Set intial properties for the button object.
+        aTextbox.Top = 140 + Count * 25
+        aTextbox.Left = 380
+        aTextbox.Width = 50
+        aTextbox.Height = 22
+        aTextbox.Tag = Me.Count
+        aTextbox.Text = "0"
+
+        '       AddHandler aTextbox.Click, AddressOf ClickHandler
+
+        Return aTextbox
+    End Function
+
+    Public Sub New(ByVal host As System.Windows.Forms.Form)
+        HostForm = host
+        Me.AddNewTextbox()
+    End Sub
+
+    Default Public ReadOnly Property Item(ByVal Index As Integer) As  _
+   System.Windows.Forms.TextBox
+        Get
+            Return CType(Me.List.Item(Index), System.Windows.Forms.TextBox)
+        End Get
+    End Property
+
+    Public Sub Remove()
+        ' Check to be sure there is a button to remove.
+        If Me.Count > 0 Then
+            ' Remove the last button added to the array from the host form 
+            ' controls collection. Note the use of the default property in 
+            ' accessing the array.
+            HostForm.Controls.Remove(Me(Me.Count - 1))
+            Me.List.RemoveAt(Me.Count - 1)
+        End If
+    End Sub
+End Class
