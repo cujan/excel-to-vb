e2c39f78aa3f7a98d106a55a663d2fe7167e7f28
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2008-12-08 19:03:29 +0000

    upravena test aplikacia

--- test_aplikacia/test_aplikacia/test_aplikacia/Form1.vb
+++ test_aplikacia/test_aplikacia/test_aplikacia/Form1.vb
@@ -1,15 +1,22 @@
 ﻿Public Class Form1
 
-    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        
 
     End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'Spz_evidenciaDataSet.clenovia' table. You can move, or remove it, as needed.
         Me.ClenoviaTableAdapter.Fill(Me.Spz_evidenciaDataSet.clenovia)
+        'TODO: This line of code loads data into the 'Spz_evidenciaDataSet.clenovia' table. You can move, or remove it, as needed.
+
+
+    End Sub
+
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
 
     End Sub
 End Class
