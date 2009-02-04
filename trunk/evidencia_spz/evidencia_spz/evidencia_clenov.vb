3d2da1838f4ce489a5d575bdc7ef84ac9c19fa2d
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-02-04 10:58:46 +0000

    updatovana databaza

--- trunk/evidencia_spz/evidencia_spz/evidencia_clenov.vb
+++ trunk/evidencia_spz/evidencia_spz/evidencia_clenov.vb
@@ -1,9 +1,7 @@
 ﻿Public Class evidencia_clenov
 
-    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        
 
     End Sub
 
@@ -12,8 +10,7 @@
         Me.Dock = DockStyle.Fill
         Me.TopLevel = False
         hlavna_aplikacia.hlavny_splitter.Panel2.Controls.Add(Me)
-        Me.ClenoviaTableAdapter.Fill(Me.Spz_evidenciaDataSet.clenovia)
-
+        
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
@@ -21,9 +18,7 @@
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
+        
     End Sub
 
     Private Sub FillByToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
@@ -33,7 +28,7 @@
 
     Private Sub HladajToolStripButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Try
-            Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
+            ' Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
         Catch ex As System.Exception
             System.Windows.Forms.MessageBox.Show(ex.Message)
         End Try
@@ -42,7 +37,7 @@
 
     Private Sub HladajToolStripButton_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Try
-            Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
+            'Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
         Catch ex As System.Exception
             System.Windows.Forms.MessageBox.Show(ex.Message)
         End Try
@@ -51,7 +46,7 @@
 
     Private Sub HladajToolStripButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Try
-            Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
+            'Me.ClenoviaTableAdapter.hladaj(Me.Spz_evidenciaDataSet.clenovia)
         Catch ex As System.Exception
             System.Windows.Forms.MessageBox.Show(ex.Message)
         End Try
