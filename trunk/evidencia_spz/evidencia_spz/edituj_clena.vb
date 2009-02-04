4c9c8da46822a0aabd8b787d634a86b386366183
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-02-04 11:14:59 +0000

    updatovana databaza

--- trunk/evidencia_spz/evidencia_spz/edituj_clena.vb
+++ trunk/evidencia_spz/evidencia_spz/edituj_clena.vb
@@ -1,22 +1,20 @@
 ﻿Public Class edituj_clena
 
     Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
+        
 
     End Sub
 
     Private Sub edituj_clena_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'Spz_evidenciaDataSet.clenovia' table. You can move, or remove it, as needed.
-        Me.ClenoviaTableAdapter.Fill(Me.Spz_evidenciaDataSet.clenovia)
+
         'TODO: This line of code loads data into the 'Pohlad_priezviskoDataSet1.clenovia' table. You can move, or remove it, as needed.
         Me.Dock = DockStyle.Fill
         Me.TopLevel = False
         hlavna_aplikacia.hlavny_splitter.Panel2.Controls.Add(Me)
 
         'TODO: This line of code loads data into the 'Spz_evidenciaDataSet.clenovia' table. You can move, or remove it, as needed.
-        Me.ClenoviaTableAdapter.Fill(Me.Spz_evidenciaDataSet.clenovia)
+
         'Me.ClenoviaBindingSource.Position = Me.ComboBox1.SelectedIndex
 
     End Sub
@@ -34,19 +32,14 @@
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
+        
     End Sub
 
-    Private Sub ClenoviaBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
-        Me.Validate()
-        Me.ClenoviaBindingSource.EndEdit()
-        Me.TableAdapterManager.UpdateAll(Me.Spz_evidenciaDataSet)
-
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+       
     End Sub
 
-    Private Sub ComboBox1_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
-        Me.ClenoviaBindingSource.Position = Me.ComboBox1.SelectedIndex
+    Private Sub ComboBox1_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        'Me.ClenoviaBindingSource.Position = Me.ComboBox1.SelectedIndex
     End Sub
 End Class
