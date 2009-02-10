88fa61eb5917da5c8c22e2b455a89e38f04f7374
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-02-10 12:17:25 +0000

    pridany ciselnik narodnost

---
+++ trunk/evidencia_spz/evidencia_spz/narodnost.vb
@@ -0,0 +1,19 @@
+﻿Public Class narodnost
+
+    Private Sub NarodnostBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NarodnostBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.NarodnostBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.NarodnostDataSet)
+
+    End Sub
+
+    Private Sub narodnost_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'NarodnostDataSet.narodnost' table. You can move, or remove it, as needed.
+        Me.NarodnostTableAdapter.Fill(Me.NarodnostDataSet.narodnost)
+
+    End Sub
+
+    Private Sub koniec_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles koniec.Click
+        Me.Close()
+    End Sub
+End Class
