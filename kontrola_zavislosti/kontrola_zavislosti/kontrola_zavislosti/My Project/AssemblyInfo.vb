c19874de62e64b36819f8d44fb5718c0be63d444
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-02-12 22:52:18 +0000

    pridana aplikacie pre kontrolu zavislosti

---
+++ kontrola_zavislosti/kontrola_zavislosti/kontrola_zavislosti/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("kontrola_zavislosti")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("UPJS")> 
+<Assembly: AssemblyProduct("kontrola_zavislosti")> 
+<Assembly: AssemblyCopyright("Copyright © UPJS 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e750acf9-e6dc-4f6f-b31a-f682a0d4a1e9")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ kontrola_zavislosti/kontrola_zavislosti/kontrola_zavislosti/hlavny_form.vb
@@ -0,0 +1,62 @@
+﻿Imports System.Windows.Forms
+Imports Microsoft.Win32
+
+Public Class hlavny_form
+    Private bInstalled As Boolean
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim message_text As String
+        Try
+            'Dim oApp As Object = CreateObject("Access.Application")
+            Dim oApp As Object = CreateObject("Microsoft.ACE.OLEDB.12.0")
+            bInstalled = True
+            message_text = "Aplikácia AccessRuntime nájdená :-)"
+            hotovo.Visible = True
+            ' Label2.Text = bInstalled.ToString
+        Catch
+            bInstalled = False
+            message_text = "Aplikácia AccessRuntime nenájdená :-("
+            'MessageBox.Show("Runtime Access 2007 nie je nainstalovany.", "Varovanie", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+        Label1.Text = message_text
+        'bInstalled = False
+        If Not bInstalled Then
+            'MessageBox.Show("Runtime Access 2007.", "Varovanie", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            'Me.Visible = False
+            Label1.Visible = False
+            Label3.Visible = True
+            Label2.Visible = True
+            Label2.BringToFront()
+
+            instaluj.Visible = True
+            zavriet.Visible = True
+            'kontrola_verzii.Show()
+            'kontrola_verzii.BringToFront()
+
+            'Else
+            '    'Me.Visible = False
+            '    Me.Hide()
+            '    kontrola_verzii.Show()
+            '    kontrola_verzii.BringToFront()
+            '    'MessageBox.Show("Runtime Access 2007 nie je nainstalovany.", "Varovanie", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+
+
+    End Sub
+
+    Private Sub instaluj_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles instaluj.Click
+        Process.Start("AccessRuntime.exe")
+        'Me.Close()
+    End Sub
+
+    Private Sub zavriet_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zavriet.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        Process.Start("http://www.microsoft.com/downloads/details.aspx?familyid=d9ae78d9-9dc6-4b38-9fa6-2c745a175aed&displaylang=en")
+    End Sub
+
+    Private Sub hotovo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles hotovo.Click
+        Me.Close()
+    End Sub
+End Class
