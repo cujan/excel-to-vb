7572895cdcfb19f4ddfa2f29954c4e209e14d7de
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-04-28 17:01:46 +0000

    tl. forma pre kontrolu brok

---
+++ trunk/evidencia_spz/evidencia_spz/chybajuce_kontrolne_strelby_brok.vb
@@ -0,0 +1,11 @@
+﻿Public Class chybajuce_kontrolne_strelby_brok
+
+    Private Sub chybajuce_kontrolne_strelby_brok_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Kontrolne_strelby_brokDataSet1.clenovia' table. You can move, or remove it, as needed.
+        Dim rok As String
+        rok = Date.Today.Year.ToString
+        Me.clenoviaTableAdapter.Fill(Me.Kontrolne_strelby_brokDataSet1.clenovia, rok)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
--- trunk/evidencia_spz/evidencia_spz/hlavna_aplikacia.vb
+++ trunk/evidencia_spz/evidencia_spz/hlavna_aplikacia.vb
@@ -293,7 +293,8 @@ Public Class hlavna_aplikacia
     End Sub
 
     Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        kontrolne_strelby_brok.show()
+        chybajuce_kontrolne_strelby_brok.Show()
+
     End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
