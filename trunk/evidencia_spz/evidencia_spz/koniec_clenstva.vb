daae948d3ede30aafe5a0db4d386b090b2f67e8d
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-04-28 08:27:20 +0000

    koniec clenstva, kontrolne strelby

---
+++ trunk/evidencia_spz/evidencia_spz/koniec_clenstva.vb
@@ -0,0 +1,15 @@
+﻿Public Class koniec_clenstva
+
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Koniec_clenstvaDataSet1)
+
+    End Sub
+
+    Private Sub koniec_clenstva_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Koniec_clenstvaDataSet1.clenovia' table. You can move, or remove it, as needed.
+        Me.ClenoviaTableAdapter.Fill(Me.Koniec_clenstvaDataSet1.clenovia)
+
+    End Sub
+End Class
---
+++ trunk/evidencia_spz/evidencia_spz/kontrolne_strelby_gula.vb
@@ -0,0 +1,15 @@
+﻿Public Class kontrolne_strelby_gula
+
+    Private Sub ClenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.Kontrolne_strelby_gulaDataSet1)
+
+    End Sub
+
+    Private Sub kontrolne_strelby_gula_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Kontrolne_strelby_gulaDataSet1.clenovia' table. You can move, or remove it, as needed.
+        Me.ClenoviaTableAdapter.Fill(Me.Kontrolne_strelby_gulaDataSet1.clenovia)
+
+    End Sub
+End Class
