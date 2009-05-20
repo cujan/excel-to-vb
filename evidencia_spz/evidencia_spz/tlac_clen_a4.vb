dab4173b7b52391d0ea4a7d08991d6956e6d429d
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-05-20 18:24:19 +0000

    clen report

--- evidencia_spz/evidencia_spz/tlac_clen_a4.vb
+++ evidencia_spz/evidencia_spz/tlac_clen_a4.vb
@@ -1,6 +1,8 @@
 ﻿Public Class tlac_clen_a4
 
     Private Sub tlac_clen_a4_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'spz_clenovia_report_a4.clenovia' table. You can move, or remove it, as needed.
+        Me.clenoviaTableAdapter.Fill(Me.spz_clenovia_report_a4.clenovia)
 
         Me.ReportViewer1.RefreshReport()
     End Sub
