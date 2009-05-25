609ba79a28d3dc18195e5d9c21635c083b99f5ba
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-05-25 20:43:47 +0000

    nahodenie generatora reg. cisel

---
+++ generacia_registracii/generacia_registracii/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class generacia_registracii
+
+    Private Sub generuj_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles generuj.Click
+
+        Dim krat As ULong
+        krat = 3691
+        Dim ica As ULong
+        ica = ica_textbox.Text
+        reg_cislo.Text = krat * ica
+
+    End Sub
+End Class
