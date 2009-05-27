ad0979a977ed127844d2f0b2ad474f220ec3359a
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-05-27 15:39:01 +0000

    pridanie formu pre zalohu databazy

--- trunk/evidencia_spz/evidencia_spz/hlavna_aplikacia.vb
+++ trunk/evidencia_spz/evidencia_spz/hlavna_aplikacia.vb
@@ -567,4 +567,10 @@ Public Class hlavna_aplikacia
     Private Sub Button1_Click_2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         zoznam_clenov_podla_zdruzenia.Show()
     End Sub
+
+    Private Sub Button2_Click_2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        zaloha_databazy.Show()
+        zaloha_databazy.BringToFront()
+
+    End Sub
 End Class
