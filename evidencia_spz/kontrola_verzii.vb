3836d75661dc80293a00eb83253661ae62b9c0ce
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-06-10 09:46:14 +0000

    oprava prvych chyb

--- evidencia_spz/kontrola_verzii.vb
+++ evidencia_spz/kontrola_verzii.vb
@@ -6,7 +6,13 @@
     End Sub
 
     Private Sub instaluj_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles instaluj.Click
-        Process.Start("AccessRuntime.exe")
+        Try
+            Process.Start("AccessRuntime.exe")
+        Catch
+            MsgBox("Nepodarilo sa nájsť program AccessRuntime, pokúsime sa stiahnuť aplikáciu z internetu.")
+            Process.Start("http://www.microsoft.com/downloads/details.aspx?familyid=d9ae78d9-9dc6-4b38-9fa6-2c745a175aed&displaylang=en")
+        End Try
+
         hlavna_aplikacia.Close()
         Me.Close()
 
