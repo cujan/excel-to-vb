d6cde47dad02bad2560f0fc1e32b16cb1e2f5dce
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-08-17 21:53:19 +0000

    zacaty projekt internat

---
+++ DM-register/internat/vychovavatelia.vb
@@ -0,0 +1,15 @@
+﻿Public Class vychovavatelia
+
+    Private Sub VychovavateliaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles VychovavateliaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.VychovavateliaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.VychovavateliaDataSet)
+        Me.VychovavateliaTableAdapter.Fill(Me.VychovavateliaDataSet.vychovavatelia)
+    End Sub
+
+    Private Sub vychovavatelia_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'VychovavateliaDataSet.vychovavatelia' table. You can move, or remove it, as needed.
+        Me.VychovavateliaTableAdapter.Fill(Me.VychovavateliaDataSet.vychovavatelia)
+
+    End Sub
+End Class
