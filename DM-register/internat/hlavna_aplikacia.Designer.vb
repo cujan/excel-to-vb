fbe441491269dc591871707133deb2bb4a57a8b9
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-09-03 17:42:49 +0000

    vytvorena grafika pre hlavnu aplikaciu

---
+++ DM-register/internat/hlavna_aplikacia.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class hlavna_aplikacia
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip
+        Me.ToolStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.hlavny_splitter = New System.Windows.Forms.SplitContainer
+        Me.nastavenia_splitter = New System.Windows.Forms.SplitContainer
+        Me.nastavenia_button = New System.Windows.Forms.Button
+        Me.nastavenia_label = New System.Windows.Forms.Label
+        Me.tl_zostavy_splitter = New System.Windows.Forms.SplitContainer
+        Me.tl_zostavy_label = New System.Windows.Forms.Label
+        Me.tl_zostavy_button = New System.Windows.Forms.Button
+        Me.StatusStrip.SuspendLayout()
+        Me.hlavny_splitter.Panel1.SuspendLayout()
+        Me.hlavny_splitter.SuspendLayout()
+        Me.nastavenia_splitter.Panel1.SuspendLayout()
+        Me.nastavenia_splitter.SuspendLayout()
+        Me.tl_zostavy_splitter.Panel1.SuspendLayout()
+        Me.tl_zostavy_splitter.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 497)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(790, 22)
+        Me.StatusStrip.TabIndex = 7
+        Me.StatusStrip.Text = "StatusStrip"
+        '
+        'ToolStripStatusLabel
+        '
+        Me.ToolStripStatusLabel.Name = "ToolStripStatusLabel"
+        Me.ToolStripStatusLabel.Size = New System.Drawing.Size(38, 17)
+        Me.ToolStripStatusLabel.Text = "Status"
+        '
+        'hlavny_splitter
+        '
+        Me.hlavny_splitter.BackColor = System.Drawing.Color.RoyalBlue
+        Me.hlavny_splitter.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.hlavny_splitter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.hlavny_splitter.IsSplitterFixed = True
+        Me.hlavny_splitter.Location = New System.Drawing.Point(0, 0)
+        Me.hlavny_splitter.Name = "hlavny_splitter"
+        '
+        'hlavny_splitter.Panel1
+        '
+        Me.hlavny_splitter.Panel1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.hlavny_splitter.Panel1.Controls.Add(Me.nastavenia_splitter)
+        Me.hlavny_splitter.Panel1.Controls.Add(Me.tl_zostavy_splitter)
+        '
+        'hlavny_splitter.Panel2
+        '
+        Me.hlavny_splitter.Panel2.BackColor = System.Drawing.Color.Lavender
+        Me.hlavny_splitter.Size = New System.Drawing.Size(790, 497)
+        Me.hlavny_splitter.SplitterDistance = 160
+        Me.hlavny_splitter.SplitterWidth = 1
+        Me.hlavny_splitter.TabIndex = 9
+        '
+        'nastavenia_splitter
+        '
+        Me.nastavenia_splitter.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.nastavenia_splitter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.nastavenia_splitter.Location = New System.Drawing.Point(7, 134)
+        Me.nastavenia_splitter.Name = "nastavenia_splitter"
+        Me.nastavenia_splitter.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'nastavenia_splitter.Panel1
+        '
+        Me.nastavenia_splitter.Panel1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.nastavenia_splitter.Panel1.Controls.Add(Me.nastavenia_button)
+        Me.nastavenia_splitter.Panel1.Controls.Add(Me.nastavenia_label)
+        '
+        'nastavenia_splitter.Panel2
+        '
+        Me.nastavenia_splitter.Panel2.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.nastavenia_splitter.Size = New System.Drawing.Size(140, 100)
+        Me.nastavenia_splitter.SplitterDistance = 25
+        Me.nastavenia_splitter.TabIndex = 0
+        '
+        'nastavenia_button
+        '
+        Me.nastavenia_button.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.nastavenia_button.FlatAppearance.BorderSize = 0
+        Me.nastavenia_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.nastavenia_button.Location = New System.Drawing.Point(110, 2)
+        Me.nastavenia_button.Margin = New System.Windows.Forms.Padding(0)
+        Me.nastavenia_button.Name = "nastavenia_button"
+        Me.nastavenia_button.Size = New System.Drawing.Size(22, 22)
+        Me.nastavenia_button.TabIndex = 1
+        Me.nastavenia_button.Text = "v"
+        Me.nastavenia_button.UseVisualStyleBackColor = False
+        '
+        'nastavenia_label
+        '
+        Me.nastavenia_label.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.nastavenia_label.BackColor = System.Drawing.Color.MediumBlue
+        Me.nastavenia_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nastavenia_label.ForeColor = System.Drawing.Color.White
+        Me.nastavenia_label.Location = New System.Drawing.Point(0, 0)
+        Me.nastavenia_label.Name = "nastavenia_label"
+        Me.nastavenia_label.Size = New System.Drawing.Size(140, 25)
+        Me.nastavenia_label.TabIndex = 0
+        Me.nastavenia_label.Text = "Nastavenia"
+        Me.nastavenia_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'tl_zostavy_splitter
+        '
+        Me.tl_zostavy_splitter.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.tl_zostavy_splitter.IsSplitterFixed = True
+        Me.tl_zostavy_splitter.Location = New System.Drawing.Point(7, 103)
+        Me.tl_zostavy_splitter.Name = "tl_zostavy_splitter"
+        Me.tl_zostavy_splitter.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'tl_zostavy_splitter.Panel1
+        '
+        Me.tl_zostavy_splitter.Panel1.Controls.Add(Me.tl_zostavy_button)
+        Me.tl_zostavy_splitter.Panel1.Controls.Add(Me.tl_zostavy_label)
+        '
+        'tl_zostavy_splitter.Panel2
+        '
+        Me.tl_zostavy_splitter.Panel2.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.tl_zostavy_splitter.Size = New System.Drawing.Size(140, 100)
+        Me.tl_zostavy_splitter.SplitterDistance = 25
+        Me.tl_zostavy_splitter.SplitterWidth = 1
+        Me.tl_zostavy_splitter.TabIndex = 1
+        '
+        'tl_zostavy_label
+        '
+        Me.tl_zostavy_label.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.tl_zostavy_label.BackColor = System.Drawing.Color.MediumBlue
+        Me.tl_zostavy_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tl_zostavy_label.ForeColor = System.Drawing.Color.White
+        Me.tl_zostavy_label.Location = New System.Drawing.Point(-1, 0)
+        Me.tl_zostavy_label.Name = "tl_zostavy_label"
+        Me.tl_zostavy_label.Size = New System.Drawing.Size(140, 25)
+        Me.tl_zostavy_label.TabIndex = 0
+        Me.tl_zostavy_label.Text = "Tl. zostavy"
+        Me.tl_zostavy_label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'tl_zostavy_button
+        '
+        Me.tl_zostavy_button.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.tl_zostavy_button.FlatAppearance.BorderSize = 0
+        Me.tl_zostavy_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.tl_zostavy_button.Location = New System.Drawing.Point(109, 2)
+        Me.tl_zostavy_button.Name = "tl_zostavy_button"
+        Me.tl_zostavy_button.Size = New System.Drawing.Size(22, 22)
+        Me.tl_zostavy_button.TabIndex = 1
+        Me.tl_zostavy_button.Text = "v"
+        Me.tl_zostavy_button.UseVisualStyleBackColor = False
+        '
+        'hlavna_aplikacia
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(790, 519)
+        Me.Controls.Add(Me.hlavny_splitter)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.IsMdiContainer = True
+        Me.Name = "hlavna_aplikacia"
+        Me.Text = "Register ubytovaných"
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.hlavny_splitter.Panel1.ResumeLayout(False)
+        Me.hlavny_splitter.ResumeLayout(False)
+        Me.nastavenia_splitter.Panel1.ResumeLayout(False)
+        Me.nastavenia_splitter.ResumeLayout(False)
+        Me.tl_zostavy_splitter.Panel1.ResumeLayout(False)
+        Me.tl_zostavy_splitter.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents ToolStripStatusLabel As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents StatusStrip As System.Windows.Forms.StatusStrip
+    Friend WithEvents hlavny_splitter As System.Windows.Forms.SplitContainer
+    Friend WithEvents nastavenia_splitter As System.Windows.Forms.SplitContainer
+    Friend WithEvents nastavenia_button As System.Windows.Forms.Button
+    Friend WithEvents nastavenia_label As System.Windows.Forms.Label
+    Friend WithEvents tl_zostavy_splitter As System.Windows.Forms.SplitContainer
+    Friend WithEvents tl_zostavy_label As System.Windows.Forms.Label
+    Friend WithEvents tl_zostavy_button As System.Windows.Forms.Button
+
+End Class
---
+++ DM-register/internat/hlavna_aplikacia.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Windows.Forms
+
+Public Class hlavna_aplikacia
+
+
+
+   
+
+    Private Sub nastavenia_button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nastavenia_button.Click
+        If nastavenia_button.Text = "v" Then
+            nastavenia_button.Text = "^"
+            nastavenia_splitter.Panel2Collapsed = False
+        Else
+            nastavenia_button.Text = "v"
+            nastavenia_splitter.Panel2Collapsed = True
+        End If
+    End Sub
+
+    Private Sub tl_zostavy_button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tl_zostavy_button.Click
+        Dim tl_zostavy_splitter_position As New System.Drawing.Point
+        If tl_zostavy_button.Text = "v" Then
+            tl_zostavy_button.Text = "^"
+            tl_zostavy_splitter.Panel2Collapsed = False
+            tl_zostavy_splitter_position.Y = tl_zostavy_splitter.Location.Y + 165
+            tl_zostavy_splitter_position.X = tl_zostavy_splitter.Location.X
+            nastavenia_splitter.Location = tl_zostavy_splitter_position
+
+        Else
+            tl_zostavy_button.Text = "v"
+            tl_zostavy_splitter.Panel2Collapsed = True
+            tl_zostavy_splitter_position.Y = tl_zostavy_splitter.Location.Y + 34
+            tl_zostavy_splitter_position.X = tl_zostavy_splitter.Location.X
+
+            nastavenia_splitter.Location = tl_zostavy_splitter_position
+
+        End If
+    End Sub
+End Class
