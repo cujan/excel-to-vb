cd0e64f3cd6fa5cddaf53fbde692c4c18407adc8
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-09-07 21:11:01 +0000

    upravy v databaze

---
+++ DM-register/internat/skupina.vb
@@ -0,0 +1,36 @@
+﻿Public Class skupina
+
+    Private Sub SkupinaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SkupinaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.SkupinaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.SkupinaDataSet)
+
+    End Sub
+
+    Private Sub skupina_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'VychovavatelDataSet.vychovavatel' table. You can move, or remove it, as needed.
+        Me.VychovavatelTableAdapter.Fill(Me.VychovavatelDataSet.vychovavatel)
+        'TODO: This line of code loads data into the 'SkupinaDataSet.skupina' table. You can move, or remove it, as needed.
+        Me.SkupinaTableAdapter.Fill(Me.SkupinaDataSet.skupina)
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim nazov As String = nazovTextBox.Text
+        Dim vychovavatel As String = vychovavatelComboBox1.SelectedValue
+
+        Dim con As New SqlCeConnection("Data Source=|DataDirectory|\db_internat.sdf")
+        con.Open()
+
+        Dim com As New SqlCeCommand("insert into skupina (nazov_skupiny,id_vychovavatel) values ('" & nazov & "','" & vychovavatel & "')", con)
+        com.ExecuteNonQuery()
+        con.Close()
+
+        'Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\spz_evidencia.accdb")
+        'con.Open()
+
+        'Dim com As New OleDb.OleDbCommand("INSERT INTO narodnost (skratka, nazov) VALUES ('" & skratka & "','" & nazov & "')", con)
+        'com.ExecuteNonQuery()
+        'con.Close()
+    End Sub
+End Class
