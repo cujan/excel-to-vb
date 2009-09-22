370a182f7a1665f202bf88464623d0e071712caa
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-09-22 19:20:37 +0000

    pridany form vychovavatelia

---
+++ trunk/DM-register/internat/vychovavatel.vb
@@ -0,0 +1,15 @@
+﻿Public Class vychovavatel
+
+    Private Sub VychovavatelBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles VychovavatelBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.VychovavatelBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.VychovavatelDataSet)
+
+    End Sub
+
+    Private Sub vychovavatel_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'VychovavatelDataSet.vychovavatel' table. You can move, or remove it, as needed.
+        Me.VychovavatelTableAdapter.Fill(Me.VychovavatelDataSet.vychovavatel)
+
+    End Sub
+End Class
