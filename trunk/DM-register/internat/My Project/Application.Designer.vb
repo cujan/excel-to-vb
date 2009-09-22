fa88999dd60c53d6858a6efa01a8f4fb4d233ba9
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-09-22 18:04:42 +0000

    Pridanie noveho menu - Sprava ubytovanych

--- trunk/DM-register/internat/My Project/Application.Designer.vb
+++ trunk/DM-register/internat/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.internat.skupina
+            Me.MainForm = Global.internat.hlavna_aplikacia
         End Sub
     End Class
 End Namespace
