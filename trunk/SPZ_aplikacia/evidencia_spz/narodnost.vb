45f1387e3349ababf5383c647fc649cadb811993
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-12-11 21:43:33 +0000

    hotovy ciselnik narodnost

--- trunk/SPZ_aplikacia/evidencia_spz/narodnost.vb
+++ trunk/SPZ_aplikacia/evidencia_spz/narodnost.vb
@@ -8,6 +8,8 @@
     End Sub
 
     Private Sub narodnost_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'SpzDataSet.narodnost' table. You can move, or remove it, as needed.
+        Me.NarodnostTableAdapter.Fill(Me.SpzDataSet.narodnost)
         'TODO: This line of code loads data into the 'NarodnostDataSet.narodnost' table. You can move, or remove it, as needed.
 
         Me.Dock = DockStyle.Fill
@@ -31,10 +33,17 @@
         Dim skratka As String = SkratkaTextBox.Text
         Dim nazov As String = NazovTextBox.Text
 
-        Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\spz_evidencia.accdb")
+        Dim con As New SqlCeConnection(pripojovaci_retazec)
         con.Open()
 
-        Dim com As New OleDb.OleDbCommand("INSERT INTO narodnost (skratka, nazov) VALUES ('" & skratka & "','" & nazov & "')", con)
+        Dim com As New SqlCeCommand("INSERT INTO narodnost (skratka, nazov) VALUES(@skratka,@nazov)", con)
+
+        With com.Parameters
+            .AddWithValue("skratka", skratka)
+            .AddWithValue("nazov", nazov)
+        End With
+
+
         com.ExecuteNonQuery()
         con.Close()
 
@@ -44,11 +53,24 @@
         Me.pridajButton.Visible = True
         Me.SkratkaTextBox.ReadOnly = True
         Me.NazovTextBox.ReadOnly = True
+        Me.SkratkaTextBox.Text = ""
+        Me.NazovTextBox.Text = ""
+
+        Me.NarodnostTableAdapter.Fill(Me.SpzDataSet.narodnost)
 
     End Sub
 
     Private Sub zmazButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zmazButton.Click
       
+        Me.Validate()
+        Me.NarodnostBindingSource.RemoveCurrent()
+        Me.TableAdapterManager.UpdateAll(Me.SpzDataSet)
+    End Sub
+
+    Private Sub NarodnostBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NarodnostBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.NarodnostBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.SpzDataSet)
 
     End Sub
 End Class
