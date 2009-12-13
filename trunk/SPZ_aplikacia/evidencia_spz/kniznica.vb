55f1f1c62cb23a272896a4142a22d9dcf8ab2ba6
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-12-13 20:16:58 +0000

    pridana kniznica

---
+++ trunk/SPZ_aplikacia/evidencia_spz/kniznica.vb
@@ -0,0 +1,9 @@
+﻿Module kniznica
+    Public Function pripojovaci_retazec() As String
+
+        Dim pripojovaci_retazec_string As String = "Data Source=|DataDirectory|\spz.sdf"
+
+
+        Return pripojovaci_retazec_string
+    End Function
+End Module
