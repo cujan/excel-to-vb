5d44b701f50595bf66a7227cbe57cbb4dfb34e20
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-12-15 20:11:24 +0000

    pridanie clena

--- trunk/SPZ_aplikacia/evidencia_spz/clenovia123.vb
+++ trunk/SPZ_aplikacia/evidencia_spz/clenovia123.vb
@@ -1,6 +1,6 @@
 ﻿Public Class clenovia123
 
-    Private Sub _123_clenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _123_clenoviaBindingNavigatorSaveItem.Click
+    Private Sub _123_clenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Me.Validate()
         Me._123_clenoviaBindingSource.EndEdit()
         Me.TableAdapterManager.UpdateAll(Me.SpzDataSet)
@@ -10,6 +10,15 @@
     Private Sub clenovia123_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'SpzDataSet._123_clenovia' table. You can move, or remove it, as needed.
         Me._123_clenoviaTableAdapter.Fill(Me.SpzDataSet._123_clenovia)
+        'TODO: This line of code loads data into the 'SpzDataSet._123_clenovia' table. You can move, or remove it, as needed.
+        Me._123_clenoviaTableAdapter.Fill(Me.SpzDataSet._123_clenovia)
+
+    End Sub
+
+    Private Sub _123_clenoviaBindingNavigatorSaveItem_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _123_clenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me._123_clenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.SpzDataSet)
 
     End Sub
 End Class
