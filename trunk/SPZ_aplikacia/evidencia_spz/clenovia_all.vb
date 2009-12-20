35c929f758eec29682b1027820f995d4d81c1f53
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2009-12-20 20:51:19 +0000

    dataset all clenovia

---
+++ trunk/SPZ_aplikacia/evidencia_spz/clenovia_all.vb
@@ -0,0 +1,15 @@
+﻿Public Class clenovia_all
+
+    Private Sub All_clenoviaBindingNavigatorSaveItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles All_clenoviaBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.All_clenoviaBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.All_clenoviaDataSet)
+
+    End Sub
+
+    Private Sub clenovia_all_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'All_clenoviaDataSet.all_clenovia' table. You can move, or remove it, as needed.
+        Me.All_clenoviaTableAdapter.Fill(Me.All_clenoviaDataSet.all_clenovia)
+
+    End Sub
+End Class
