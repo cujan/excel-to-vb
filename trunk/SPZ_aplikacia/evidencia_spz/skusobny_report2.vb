57f61b652ed8b52e9f8271cd0bcc90ac135830b0
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-02 13:23:58 +0000

    zvysne subory

---
+++ trunk/SPZ_aplikacia/evidencia_spz/skusobny_report2.vb
@@ -0,0 +1,9 @@
+﻿Public Class skusobny_report2
+
+    Private Sub skusobny_report2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'kartyClenov.karty_clenov' table. You can move, or remove it, as needed.
+        Me.karty_clenovTableAdapter.Fill(Me.kartyClenov.karty_clenov)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
