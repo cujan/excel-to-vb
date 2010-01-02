cff66422b64664abad500f04da688632618f8846
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-02 12:36:17 +0000

    pridana karta clenov

--- SPZ_aplikacia/evidencia_spz/clenovia_all.vb
+++ SPZ_aplikacia/evidencia_spz/clenovia_all.vb
@@ -9,66 +9,7 @@
 
     Private Sub clenovia_all_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-        'vytvori select pre kopirovanie tabuliek do velkej
-
-        Dim query As String = "SELECT [ICO_clenovia] FROM zdruzenia"
-        Dim conn As New SqlCeConnection(pripojovaci_retazec)
-        Dim cmd As New SqlCeCommand(query, conn)
-        Dim i As Integer
-        Dim val1 As String
-        Dim unionSelect As String
-        Dim fromTabulky As String
-
-        fromTabulky = ""
-        unionSelect = "insert into ""all_clenovia"" "
-
-
-        conn.Open()
-        Dim rdr As SqlCeDataReader = cmd.ExecuteReader()
-        i = 1
-        Try
-            ' Iterate through the results
-            '
-            While rdr.Read()
-                val1 = rdr.GetString(0)
-                If i = 1 Then
-                    fromTabulky = fromTabulky + " select * from """ + val1 + """"
-                    i = i + 1
-                Else
-                    fromTabulky = fromTabulky + " union select * from """ + val1 + """ "
-                End If
-            End While
-        Finally
-            ' Always call Close when done reading
-            '
-            rdr.Close()
-
-            ' Always call Close when done reading
-            '
-            conn.Close()
-        End Try
-
-        unionSelect = unionSelect + fromTabulky + " "
-        'MsgBox(unionSelect)
-
-        'najprv vymaz tabulku all_clenovia
-        Dim truncateTable As String
-        truncateTable = "DELETE FROM ""all_clenovia"" "
-        Dim connectionTruncate As New SqlCeConnection(pripojovaci_retazec)
-        connectionTruncate.Open()
-        Dim commandTruncate As New SqlCeCommand(truncateTable, connectionTruncate)
-        commandTruncate.ExecuteNonQuery()
-        connectionTruncate.Close()
-
-
-        'vkopiruj tabulky do all_clenova pouzij unionSelect query
-        Dim sqlReturn As New Integer
-        Dim connection As New SqlCeConnection(pripojovaci_retazec)
-        connection.Open()
-        Dim command As New SqlCeCommand(unionSelect, connection)
-        sqlReturn = command.ExecuteNonQuery()
-        connection.Close()
-
+        hlavna_aplikacia.vytvor_all_clenovia()
 
 
         Me.Dock = DockStyle.Fill
--- SPZ_aplikacia/evidencia_spz/karty_clenov.vb
+++ SPZ_aplikacia/evidencia_spz/karty_clenov.vb
@@ -1,6 +1,10 @@
 ﻿Public Class karty_clenov
 
     Private Sub karty_clenov_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'kartyClenov.karty_clenov' table. You can move, or remove it, as needed.
+        hlavna_aplikacia.vytvor_all_clenovia()
+
+        Me.karty_clenovTableAdapter.Fill(Me.kartyClenov.karty_clenov)
 
         Me.ReportViewer1.RefreshReport()
     End Sub
