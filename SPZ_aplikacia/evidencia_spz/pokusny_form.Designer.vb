4a4a41e8f7a4c16a2308eace7c87fa66ebdc7bc0
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-10 22:23:03 +0000

    pridana mdf databaza

--- SPZ_aplikacia/evidencia_spz/pokusny_form.Designer.vb
+++ SPZ_aplikacia/evidencia_spz/pokusny_form.Designer.vb
@@ -22,8 +22,16 @@ Partial Class pokusny_form
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'pokusny_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(941, 443)
+        Me.Name = "pokusny_form"
         Me.Text = "pokusny_form"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
