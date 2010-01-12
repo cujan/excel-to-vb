75170259ee6f12e1233cc1974e0bdec7508ce0d8
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-12 12:13:08 +0000

    Pridany dataset pre kont. strelby

---
+++ trunk/SPZ_aplikacia/evidencia_spz/zoznam_kont_strelby.vb
@@ -0,0 +1,13 @@
+﻿Partial Class zoznam_kont_strelby
+    Partial Class zoznam_kont_strelbyDataTable
+
+        Private Sub zoznam_kont_strelbyDataTable_ColumnChanging(ByVal sender As System.Object, ByVal e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            If (e.Column.ColumnName = Me.zdruzenie_nazovColumn.ColumnName) Then
+                'Add user code here
+            End If
+
+        End Sub
+
+    End Class
+
+End Class
