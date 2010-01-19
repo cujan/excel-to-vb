021a95d2483be2f027a71e8539c7db4e84f6b7c7
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-19 22:01:17 +0000

    uprava tl. zoznamov + dorobenie zoznamu pre narodeniny

--- SPZ_aplikacia/evidencia_spz/zoznam_narodeniny.Designer.vb
+++ SPZ_aplikacia/evidencia_spz/zoznam_narodeniny.Designer.vb
@@ -23,21 +23,29 @@ Partial Class zoznam_narodeniny
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource
         Me.narodeniny_check = New System.Windows.Forms.CheckBox
         Me.mesiace_combo = New System.Windows.Forms.ComboBox
         Me.zdruzenia_check = New System.Windows.Forms.CheckBox
         Me.zdruzenia_combo = New System.Windows.Forms.ComboBox
-        Me.Zoznam_zdruzeni_combo = New evidencia_spz.zoznam_zdruzeni_combo
         Me.ZdruzeniaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Zoznam_zdruzeni_combo = New evidencia_spz.zoznam_zdruzeni_combo
         Me.ZdruzeniaTableAdapter = New evidencia_spz.zoznam_zdruzeni_comboTableAdapters.zdruzeniaTableAdapter
-        CType(Me.Zoznam_zdruzeni_combo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer
+        Me.narodeniny_label = New System.Windows.Forms.Label
+        Me.narodeniny = New evidencia_spz.narodeniny
+        Me.all_clenoviaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.all_clenoviaTableAdapter = New evidencia_spz.narodeninyTableAdapters.all_clenoviaTableAdapter
         CType(Me.ZdruzeniaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Zoznam_zdruzeni_combo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.narodeniny, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.all_clenoviaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'narodeniny_check
         '
         Me.narodeniny_check.AutoSize = True
-        Me.narodeniny_check.Location = New System.Drawing.Point(12, 12)
+        Me.narodeniny_check.Location = New System.Drawing.Point(366, 8)
         Me.narodeniny_check.Name = "narodeniny_check"
         Me.narodeniny_check.Size = New System.Drawing.Size(133, 17)
         Me.narodeniny_check.TabIndex = 0
@@ -49,7 +57,7 @@ Partial Class zoznam_narodeniny
         Me.mesiace_combo.Enabled = False
         Me.mesiace_combo.FormattingEnabled = True
         Me.mesiace_combo.Items.AddRange(New Object() {"Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December"})
-        Me.mesiace_combo.Location = New System.Drawing.Point(12, 35)
+        Me.mesiace_combo.Location = New System.Drawing.Point(366, 31)
         Me.mesiace_combo.Name = "mesiace_combo"
         Me.mesiace_combo.Size = New System.Drawing.Size(133, 21)
         Me.mesiace_combo.TabIndex = 1
@@ -57,7 +65,7 @@ Partial Class zoznam_narodeniny
         'zdruzenia_check
         '
         Me.zdruzenia_check.AutoSize = True
-        Me.zdruzenia_check.Location = New System.Drawing.Point(223, 12)
+        Me.zdruzenia_check.Location = New System.Drawing.Point(521, 8)
         Me.zdruzenia_check.Name = "zdruzenia_check"
         Me.zdruzenia_check.Size = New System.Drawing.Size(139, 17)
         Me.zdruzenia_check.TabIndex = 2
@@ -70,39 +78,78 @@ Partial Class zoznam_narodeniny
         Me.zdruzenia_combo.DisplayMember = "nazov"
         Me.zdruzenia_combo.Enabled = False
         Me.zdruzenia_combo.FormattingEnabled = True
-        Me.zdruzenia_combo.Location = New System.Drawing.Point(223, 35)
+        Me.zdruzenia_combo.Location = New System.Drawing.Point(521, 31)
         Me.zdruzenia_combo.Name = "zdruzenia_combo"
         Me.zdruzenia_combo.Size = New System.Drawing.Size(139, 21)
         Me.zdruzenia_combo.TabIndex = 3
         Me.zdruzenia_combo.ValueMember = "nazov"
-        '
-        'Zoznam_zdruzeni_combo
-        '
-        Me.Zoznam_zdruzeni_combo.DataSetName = "zoznam_zdruzeni_combo"
-        Me.Zoznam_zdruzeni_combo.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'ZdruzeniaBindingSource
         '
         Me.ZdruzeniaBindingSource.DataMember = "zdruzenia"
         Me.ZdruzeniaBindingSource.DataSource = Me.Zoznam_zdruzeni_combo
         '
+        'Zoznam_zdruzeni_combo
+        '
+        Me.Zoznam_zdruzeni_combo.DataSetName = "zoznam_zdruzeni_combo"
+        Me.Zoznam_zdruzeni_combo.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
         'ZdruzeniaTableAdapter
         '
         Me.ZdruzeniaTableAdapter.ClearBeforeFill = True
+        '
+        'ReportViewer1
+        '
+        ReportDataSource1.Name = "narodeniny_all_clenovia"
+        ReportDataSource1.Value = Me.all_clenoviaBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "evidencia_spz.zoznam_narodeniny_report.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(12, 58)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(943, 512)
+        Me.ReportViewer1.TabIndex = 4
+        '
+        'narodeniny_label
+        '
+        Me.narodeniny_label.AutoSize = True
+        Me.narodeniny_label.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.narodeniny_label.Location = New System.Drawing.Point(26, 15)
+        Me.narodeniny_label.Name = "narodeniny_label"
+        Me.narodeniny_label.Size = New System.Drawing.Size(191, 24)
+        Me.narodeniny_label.TabIndex = 5
+        Me.narodeniny_label.Text = "Narodeniny v mesiaci"
+        '
+        'narodeniny
+        '
+        Me.narodeniny.DataSetName = "narodeniny"
+        Me.narodeniny.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'all_clenoviaBindingSource
+        '
+        Me.all_clenoviaBindingSource.DataMember = "all_clenovia"
+        Me.all_clenoviaBindingSource.DataSource = Me.narodeniny
+        '
+        'all_clenoviaTableAdapter
+        '
+        Me.all_clenoviaTableAdapter.ClearBeforeFill = True
         '
         'zoznam_narodeniny
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(790, 528)
+        Me.ClientSize = New System.Drawing.Size(967, 582)
+        Me.Controls.Add(Me.narodeniny_label)
+        Me.Controls.Add(Me.ReportViewer1)
         Me.Controls.Add(Me.zdruzenia_combo)
         Me.Controls.Add(Me.zdruzenia_check)
         Me.Controls.Add(Me.mesiace_combo)
         Me.Controls.Add(Me.narodeniny_check)
         Me.Name = "zoznam_narodeniny"
         Me.Text = "Zoznam narodenín členov"
-        CType(Me.Zoznam_zdruzeni_combo, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.ZdruzeniaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Zoznam_zdruzeni_combo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.narodeniny, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.all_clenoviaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -114,4 +161,9 @@ Partial Class zoznam_narodeniny
     Friend WithEvents Zoznam_zdruzeni_combo As evidencia_spz.zoznam_zdruzeni_combo
     Friend WithEvents ZdruzeniaBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents ZdruzeniaTableAdapter As evidencia_spz.zoznam_zdruzeni_comboTableAdapters.zdruzeniaTableAdapter
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents narodeniny_label As System.Windows.Forms.Label
+    Friend WithEvents all_clenoviaBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents narodeniny As evidencia_spz.narodeniny
+    Friend WithEvents all_clenoviaTableAdapter As evidencia_spz.narodeninyTableAdapters.all_clenoviaTableAdapter
 End Class
--- SPZ_aplikacia/evidencia_spz/zoznam_narodeniny.vb
+++ SPZ_aplikacia/evidencia_spz/zoznam_narodeniny.vb
@@ -7,20 +7,30 @@
             mesiace_combo.SelectedValue = ""
         Else
             mesiace_combo.Enabled = False
-            mesiac = "%"
+            mesiac = Date.Today.Month
         End If
     End Sub
 
     Private Sub mesiace_combo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mesiace_combo.SelectedIndexChanged
         mesiac = (mesiace_combo.SelectedIndex + 1).ToString
-        MsgBox(mesiac + " " + mesiace_combo.SelectedText)
+
+
+        narodeniny_label.Text = "Narodeniny v " + mesiac + ". mesiaci"
+        Me.all_clenoviaTableAdapter.Fill(Me.narodeniny.all_clenovia, mesiac, zdruzenie)
+        Me.ReportViewer1.RefreshReport()
     End Sub
 
     Private Sub zoznam_narodeniny_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'narodeniny.all_clenovia' table. You can move, or remove it, as needed.
+
         'TODO: This line of code loads data into the 'Zoznam_zdruzeni_combo.zdruzenia' table. You can move, or remove it, as needed.
         Me.ZdruzeniaTableAdapter.Fill(Me.Zoznam_zdruzeni_combo.zdruzenia)
-        mesiac = "%"
+        mesiac = Date.Today.Month
         zdruzenie = "%"
+        hlavna_aplikacia.vytvor_all_clenovia()
+        narodeniny_label.Text = "Narodeniny v " + mesiac + ". mesiaci"
+        Me.all_clenoviaTableAdapter.Fill(Me.narodeniny.all_clenovia, mesiac, zdruzenie)
+        Me.ReportViewer1.RefreshReport()
     End Sub
 
     Private Sub zdruzenia_check_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zdruzenia_check.CheckedChanged
@@ -35,6 +45,9 @@
 
     Private Sub zdruzenia_combo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zdruzenia_combo.SelectedIndexChanged
         zdruzenie = zdruzenia_combo.SelectedValue
-        MsgBox(zdruzenie)
+
+
+        Me.all_clenoviaTableAdapter.Fill(Me.narodeniny.all_clenovia, mesiac, zdruzenie)
+        Me.ReportViewer1.RefreshReport()
     End Sub
 End Class
