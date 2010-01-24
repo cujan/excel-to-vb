f7311bfcfa32b960278ec2fa50c0131cb513b80c
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-01-24 22:05:25 +0000

    mazanie zdruzenie a clen

--- SPZ_aplikacia/evidencia_spz/zdruzenie.Designer.vb
+++ SPZ_aplikacia/evidencia_spz/zdruzenie.Designer.vb
@@ -88,6 +88,7 @@ Partial Class zdruzenie
         Me.zavriet_kartu_button = New System.Windows.Forms.Button
         Me.Label1 = New System.Windows.Forms.Label
         Me.Label2 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
         CType(Me.SpzDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ZdruzeniaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ZdruzeniaBindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -242,6 +243,7 @@ Partial Class zdruzenie
         Me.ZdruzeniaDataGridView.DataSource = Me.ZdruzeniaBindingSource
         Me.ZdruzeniaDataGridView.Location = New System.Drawing.Point(51, 71)
         Me.ZdruzeniaDataGridView.Name = "ZdruzeniaDataGridView"
+        Me.ZdruzeniaDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.ZdruzeniaDataGridView.Size = New System.Drawing.Size(664, 257)
         Me.ZdruzeniaDataGridView.TabIndex = 1
         '
@@ -532,12 +534,22 @@ Partial Class zdruzenie
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Label2"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(561, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "zmaz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'zdruzenie
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.PaleGreen
         Me.ClientSize = New System.Drawing.Size(759, 602)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.zavriet_kartu_button)
@@ -620,4 +632,5 @@ Partial Class zdruzenie
     Friend WithEvents zavriet_kartu_button As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
