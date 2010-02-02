39d073143213dc7f4e1cf1996aa21f2b4300b778
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-02-02 20:20:36 +0000

    prevod

---
+++ prevod_databazy/prevod_databazy/prevod_databazy/kniznica.vb
@@ -0,0 +1,17 @@
+﻿Module kniznica
+    Public Function pripojovaci_retazec_sdf() As String
+
+        Dim pripojovaci_retazec_string As String = "Data Source=|DataDirectory|\databza_sdf.sdf"
+
+
+        Return pripojovaci_retazec_string
+    End Function
+
+    Public Function pripojovaci_retazec_accdb() As String
+
+        Dim pripojovaci_retazec_string As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\spz_evidencia_accb.accdb"
+
+
+        Return pripojovaci_retazec_string
+    End Function
+End Module
