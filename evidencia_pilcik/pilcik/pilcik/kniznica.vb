85e66a62894a50508b6df38453118914061cc943
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-03-10 21:32:43 +0000

    update pilcik

---
+++ evidencia_pilcik/pilcik/pilcik/kniznica.vb
@@ -0,0 +1,10 @@
+﻿Module kniznica
+    Public Function pripojovaci_retazec() As String
+
+        Dim pripojovaci_retazec_string As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\pilcik_db.accdb"
+
+
+        Return pripojovaci_retazec_string
+    End Function
+
+End Module
---
+++ evidencia_pilcik/pilcik/pilcik/uvodna_obrazovka.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class uvodna_obrazovka
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'uvodna_obrazovka
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(945, 501)
+        Me.Name = "uvodna_obrazovka"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "uvodna_obrazovka"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
