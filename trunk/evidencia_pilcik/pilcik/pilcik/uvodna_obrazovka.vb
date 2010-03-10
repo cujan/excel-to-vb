0a153e3f33c371f608c318ed77fa50cf8b8dbd1b
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-03-10 21:32:43 +0000

    update pilcik

---
+++ trunk/evidencia_pilcik/pilcik/pilcik/uvodna_obrazovka.vb
@@ -0,0 +1,6 @@
+﻿Public Class uvodna_obrazovka
+
+    Private Sub uvodna_obrazovka_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = hlavna_aplikacia
+    End Sub
+End Class
