51c8a58279bd1bc5359d60738bf03c839b31bcb6
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-04-15 13:30:02 +0000

    update pilcik

--- evidencia_pilcik/pilcik/pilcik/clen_kurzu_pridaj.vb
+++ evidencia_pilcik/pilcik/pilcik/clen_kurzu_pridaj.vb
@@ -10,14 +10,33 @@
         Dim vybrany_kurz_id As Integer = kurz_novy.Label2.Text
         Dim vybrany_clen_id As Integer = Me.ComboBox1.SelectedValue
 
-        Dim con As New SqlCeConnection(pripojovaci_retazec)
-        Dim com As New SqlCeCommand("INSERT INTO clenovia_kurzu (kurz_id, clen_id) VALUES (@kurz_id,@clen_id)", con)
-        com.Parameters.AddWithValue("kurz_id", vybrany_kurz_id)
-        com.Parameters.AddWithValue("clen_id", vybrany_clen_id)
-        con.Open()
-        com.ExecuteNonQuery()
-        con.Close()
-        kurz_detail.Clenovia_kurzuTableAdapter.FillBy_parameter(kurz_detail.Clenovia_kurzuDataSet.clenovia_kurzu, vybrany_kurz_id)
-        kurz_novy.KurzTableAdapter.Fill(kurz_novy.Kurz_pocetDataSet.kurz)
+
+        'kontroluje ci nahodou nie je pridavany clen uz v kurze
+        Dim con1 As New SqlCeConnection(pripojovaci_retazec)
+        Dim com1 As New SqlCeCommand("SELECT COUNT(*) AS Expr1 FROM clenovia_kurzu WHERE (kurz_id = @kurz_id) AND (clen_id =@clen_id)", con1)
+        com1.Parameters.AddWithValue("kurz_id", vybrany_kurz_id)
+        com1.Parameters.AddWithValue("clen_id", vybrany_clen_id)
+        con1.Open()
+        Dim vyskyt_clena As Integer
+        vyskyt_clena = com1.ExecuteScalar()
+        con1.Close()
+
+
+
+
+        If vyskyt_clena = 0 Then
+            'pridava clena do kurzu
+            Dim con As New SqlCeConnection(pripojovaci_retazec)
+            Dim com As New SqlCeCommand("INSERT INTO clenovia_kurzu (kurz_id, clen_id) VALUES (@kurz_id,@clen_id)", con)
+            com.Parameters.AddWithValue("kurz_id", vybrany_kurz_id)
+            com.Parameters.AddWithValue("clen_id", vybrany_clen_id)
+            con.Open()
+            com.ExecuteNonQuery()
+            con.Close()
+            kurz_detail.Clenovia_kurzuTableAdapter.FillBy_parameter(kurz_detail.Clenovia_kurzuDataSet.clenovia_kurzu, vybrany_kurz_id)
+            kurz_novy.KurzTableAdapter.Fill(kurz_novy.Kurz_pocetDataSet.kurz)
+        Else
+            MsgBox("Tento človek sa už v kurze nachádza!!!")
+        End If
     End Sub
 End Class
--- evidencia_pilcik/pilcik/pilcik/tz_protokol.Designer.vb
+++ evidencia_pilcik/pilcik/pilcik/tz_protokol.Designer.vb
@@ -24,39 +24,85 @@ Partial Class tz_protokol
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container
         Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource
+        Dim ReportDataSource2 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource
+        Dim ReportDataSource3 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer
-        Me.protokolDataSet = New pilcik.protokolDataSet
-        Me.protokolDataTableBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.protokolDataTableTableAdapter = New pilcik.protokolDataSetTableAdapters.protokolDataTableTableAdapter
-        CType(Me.protokolDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.protokolDataTableBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pilcikdbDataSet = New pilcik.pilcikdbDataSet
+        Me.skusobna_komisiaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.skusobna_komisiaTableAdapter = New pilcik.pilcikdbDataSetTableAdapters.skusobna_komisiaTableAdapter
+        Me.KurzBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Protokol_kurzDataSet = New pilcik.protokol_kurzDataSet
+        Me.KurzTableAdapter = New pilcik.protokol_kurzDataSetTableAdapters.kurzTableAdapter
+        Me.ProtokolDataSet = New pilcik.protokolDataSet
+        Me.OsobaBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.OsobaTableAdapter = New pilcik.protokolDataSetTableAdapters.osobaTableAdapter
+        CType(Me.pilcikdbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.skusobna_komisiaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KurzBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Protokol_kurzDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProtokolDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OsobaBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ReportViewer1
         '
         Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
-        ReportDataSource1.Name = "protokolDataSet_protokolDataTable"
-        ReportDataSource1.Value = Me.protokolDataTableBindingSource
+        ReportDataSource1.Name = "pilcikdbDataSet_skusobna_komisia"
+        ReportDataSource1.Value = Me.skusobna_komisiaBindingSource
+        ReportDataSource2.Name = "protokol_kurzDataSet_kurz"
+        ReportDataSource2.Value = Me.KurzBindingSource
+        ReportDataSource3.Name = "protokolDataSet_osoba"
+        ReportDataSource3.Value = Me.OsobaBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource2)
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource3)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "pilcik.protokol_Report.rdlc"
         Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.Size = New System.Drawing.Size(726, 756)
         Me.ReportViewer1.TabIndex = 0
         '
-        'protokolDataSet
+        'pilcikdbDataSet
         '
-        Me.protokolDataSet.DataSetName = "protokolDataSet"
-        Me.protokolDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        Me.pilcikdbDataSet.DataSetName = "pilcikdbDataSet"
+        Me.pilcikdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        'protokolDataTableBindingSource
+        'skusobna_komisiaBindingSource
         '
-        Me.protokolDataTableBindingSource.DataMember = "protokolDataTable"
-        Me.protokolDataTableBindingSource.DataSource = Me.protokolDataSet
+        Me.skusobna_komisiaBindingSource.DataMember = "skusobna_komisia"
+        Me.skusobna_komisiaBindingSource.DataSource = Me.pilcikdbDataSet
         '
-        'protokolDataTableTableAdapter
+        'skusobna_komisiaTableAdapter
         '
-        Me.protokolDataTableTableAdapter.ClearBeforeFill = True
+        Me.skusobna_komisiaTableAdapter.ClearBeforeFill = True
+        '
+        'KurzBindingSource
+        '
+        Me.KurzBindingSource.DataMember = "kurz"
+        Me.KurzBindingSource.DataSource = Me.Protokol_kurzDataSet
+        '
+        'Protokol_kurzDataSet
+        '
+        Me.Protokol_kurzDataSet.DataSetName = "protokol_kurzDataSet"
+        Me.Protokol_kurzDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'KurzTableAdapter
+        '
+        Me.KurzTableAdapter.ClearBeforeFill = True
+        '
+        'ProtokolDataSet
+        '
+        Me.ProtokolDataSet.DataSetName = "protokolDataSet"
+        Me.ProtokolDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'OsobaBindingSource
+        '
+        Me.OsobaBindingSource.DataMember = "osoba"
+        Me.OsobaBindingSource.DataSource = Me.ProtokolDataSet
+        '
+        'OsobaTableAdapter
+        '
+        Me.OsobaTableAdapter.ClearBeforeFill = True
         '
         'tz_protokol
         '
@@ -67,14 +113,24 @@ Partial Class tz_protokol
         Me.Name = "tz_protokol"
         Me.Text = "tz_protokol"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.protokolDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.protokolDataTableBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pilcikdbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.skusobna_komisiaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KurzBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Protokol_kurzDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProtokolDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OsobaBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
-    Friend WithEvents protokolDataTableBindingSource As System.Windows.Forms.BindingSource
-    Friend WithEvents protokolDataSet As pilcik.protokolDataSet
-    Friend WithEvents protokolDataTableTableAdapter As pilcik.protokolDataSetTableAdapters.protokolDataTableTableAdapter
+    Friend WithEvents skusobna_komisiaBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents pilcikdbDataSet As pilcik.pilcikdbDataSet
+    Friend WithEvents skusobna_komisiaTableAdapter As pilcik.pilcikdbDataSetTableAdapters.skusobna_komisiaTableAdapter
+    Friend WithEvents Protokol_kurzDataSet As pilcik.protokol_kurzDataSet
+    Friend WithEvents KurzBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents KurzTableAdapter As pilcik.protokol_kurzDataSetTableAdapters.kurzTableAdapter
+    Friend WithEvents OsobaBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ProtokolDataSet As pilcik.protokolDataSet
+    Friend WithEvents OsobaTableAdapter As pilcik.protokolDataSetTableAdapters.osobaTableAdapter
 
 End Class
