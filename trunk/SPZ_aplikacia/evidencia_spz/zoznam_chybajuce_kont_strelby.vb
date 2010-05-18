7d1d763fa86cc0821b6b0589b28808f3cd73b91a
kocan.marek <kocan.marek@70332664-8a54-11dd-8937-b3a01dae0fbe>
2010-05-18 20:52:02 +0000

    uprava tl. zostavy kont. strelby

--- trunk/SPZ_aplikacia/evidencia_spz/zoznam_chybajuce_kont_strelby.vb
+++ trunk/SPZ_aplikacia/evidencia_spz/zoznam_chybajuce_kont_strelby.vb
@@ -15,6 +15,11 @@
     End Sub
 
     Private Sub chybajuce_kont_strelby_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'zoznam_kont_strelby.all_clenovia' table. You can move, or remove it, as needed.
+        Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kont_strelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+        'TODO: This line of code loads data into the 'zoznam_kont_strelby.all_clenovia' table. You can move, or remove it, as needed.
+        'TODO: This line of code loads data into the 'Zoznam_zdruzeni_combo.zdruzenia' table. You can move, or remove it, as needed.
+        'TODO: This line of code loads data into the 'zoznam_kont_strelby.all_clenovia' table. You can move, or remove it, as needed.
         'TODO: This line of code loads data into the 'Zoznam_zdruzeni_combo.zdruzenia' table. You can move, or remove it, as needed.
         Me.ZdruzeniaTableAdapter.Fill(Me.Zoznam_zdruzeni_combo.zdruzenia)
         'TODO: This line of code loads data into the 'zoznam_kontStrelby.all_clenovia' table. You can move, or remove it, as needed.
@@ -23,7 +28,7 @@
         podla_zbrane_gula = "ÁNO"
         podla_zbrane_brok = "ÁNO"
         zdruzenie = "%"
-        Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kontStrelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+
 
         Me.ReportViewer1.RefreshReport()
     End Sub
@@ -32,12 +37,12 @@
         If podla_zbrane_combo.SelectedIndex = 0 Then
             podla_zbrane_brok = "ÁNO"
             podla_zbrane_gula = "Vypnut"
-            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kontStrelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kont_strelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
             Me.ReportViewer1.RefreshReport()
         ElseIf podla_zbrane_combo.SelectedIndex = 1 Then
             podla_zbrane_brok = "Vypnut"
             podla_zbrane_gula = "ÁNO"
-            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kontStrelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kont_strelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
             Me.ReportViewer1.RefreshReport()
         End If
     End Sub
@@ -56,13 +61,13 @@
     Private Sub zdruzenie_combo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles zdruzenie_combo.SelectedIndexChanged
         If zdruzenie_combo.SelectedIndex <> -1 Then
             zdruzenie = zdruzenie_combo.SelectedValue
-            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kontStrelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+            Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kont_strelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
             Me.ReportViewer1.RefreshReport()
         End If
     End Sub
 
     Private Sub nacitat_button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nacitat_button.Click
-        Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kontStrelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
+        Me.all_clenoviaTableAdapter.Fill(Me.zoznam_kont_strelby.all_clenovia, podla_zbrane_brok, zdruzenie, podla_zbrane_gula, zdruzenie)
         Me.ReportViewer1.RefreshReport()
     End Sub
 End Class
