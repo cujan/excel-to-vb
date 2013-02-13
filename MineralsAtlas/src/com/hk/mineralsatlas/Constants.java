7d9738f1d413a38b7324e09c20802f741fa11572
kocan.marek <kocan.marek@gmail.com>
2013-02-13 08:18:24 +0000

    dalsia action pridana

--- MineralsAtlas/src/com/hk/mineralsatlas/Constants.java
+++ MineralsAtlas/src/com/hk/mineralsatlas/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
 
 	public static final String EXTRA_ACTION = "action";
+	public static final String EXTRA_ACTION_NAME = "action_name";	
 	public static final String EXTRA_URL = "url";
 	public static final String EXTRA_PATH = "path";
 	public static final String MINERALS_LIST = "minerals_list";
@@ -12,8 +13,10 @@ public class Constants {
 	public static final String ACTION_MINERALS_LIST ="index.php";
 	public static final String ACTION_LOCATIONS_LIST = "locations.php";	
 	public static final String ACTION_MAP ="map?id=";	
+	public static final String ACTION_PHOTOS ="getMineralsPhoto.php?id=";	
 	
 	public static final int TIMEOUT_CONNECTION= 15000;
 	public static final String ACTION_MINERALS_DETAIL = "mineralsDetail.php?id=";
+
 
 }
