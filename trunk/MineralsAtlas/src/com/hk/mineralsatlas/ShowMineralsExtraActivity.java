9362b84f51ece938ae61fdd54bce0e40841ffb02
kocan.marek@gmail.com <kocan.marek@gmail.com@70332664-8a54-11dd-8937-b3a01dae0fbe>
2013-02-20 20:59:21 +0000

    vyhodeny text v web_layout

--- trunk/MineralsAtlas/src/com/hk/mineralsatlas/ShowMineralsExtraActivity.java
+++ trunk/MineralsAtlas/src/com/hk/mineralsatlas/ShowMineralsExtraActivity.java
@@ -1,6 +1,5 @@
 package com.hk.mineralsatlas;
 
-
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -13,9 +12,7 @@ import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class ShowMineralsExtraActivity extends Activity {
 
@@ -26,13 +23,12 @@ public class ShowMineralsExtraActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.web_layout);
 		String path = getIntent().getStringExtra(Constants.EXTRA_PATH);
-		String actionKey = getIntent().getStringExtra(Constants.EXTRA_ACTION_KEY);		
+		String actionKey = getIntent().getStringExtra(
+				Constants.EXTRA_ACTION_KEY);
 		String url = getIntent().getStringExtra(Constants.EXTRA_URL);
 		Resources res = getResources();
 		// Imageview to show
 		WebView web = (WebView) findViewById(R.id.web);
-		TextView text = (TextView) findViewById(R.id.text);
-		text.setText(res.getString(R.string.progress_extra_text, actionKey));
 
 		// Let's display the progress in the activity title bar, like the
 		// browser app does.
@@ -40,41 +36,40 @@ public class ShowMineralsExtraActivity extends Activity {
 		WebSettings webSettings = web.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		webSettings.setBuiltInZoomControls(true);
-//		web.setHttpAuthUsernamePassword(url, "",
-//				SettingsActivity.getLogin(this), SettingsActivity.getPass(this));
+		// web.setHttpAuthUsernamePassword(url, "",
+		// SettingsActivity.getLogin(this), SettingsActivity.getPass(this));
 
 		final Activity activity = this;
 
 		web.setWebViewClient(new WebViewClient() {
-//			@Override
-//			public void onReceivedHttpAuthRequest(WebView view,
-//					HttpAuthHandler handler, String host, String realm) {
-//				handler.proceed(SettingsActivity
-//						.getLogin(ShowMineralsExtraActivity.this),
-//						SettingsActivity
-//								.getPass(ShowMineralsExtraActivity.this));
-//			}
+			// @Override
+			// public void onReceivedHttpAuthRequest(WebView view,
+			// HttpAuthHandler handler, String host, String realm) {
+			// handler.proceed(SettingsActivity
+			// .getLogin(ShowMineralsExtraActivity.this),
+			// SettingsActivity
+			// .getPass(ShowMineralsExtraActivity.this));
+			// }
 
 			@Override
 			@TargetApi(Build.VERSION_CODES.FROYO)
 			public void onReceivedSslError(WebView view,
 					SslErrorHandler handler, SslError error) {
 				handler.proceed();
-//				Toast.makeText(activity, "SSL Error " + error.toString(),
-//						Toast.LENGTH_LONG).show();				
+				// Toast.makeText(activity, "SSL Error " + error.toString(),
+				// Toast.LENGTH_LONG).show();
 			}
-			
+
 			@Override
 			public void onReceivedError(WebView view, int errorCode,
 					String description, String failingUrl) {
 				Toast.makeText(activity, "Oh no! " + description,
 						Toast.LENGTH_LONG).show();
-			}			
-		});		
-		
-		web.loadUrl(url + path+ actionKey);
+			}
+		});
+
+		web.loadUrl(url + path + actionKey);
 
 	}
-
 
 }
