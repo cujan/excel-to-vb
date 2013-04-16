ca2653100292d5a4e2c0d667bc71e28bd27ad4e7
kocan.marek@gmail.com <kocan.marek@gmail.com@70332664-8a54-11dd-8937-b3a01dae0fbe>
2013-04-16 22:12:21 +0000

    novy projekt pre kalendar chovu matiek

---
+++ trunk/QueenB/src/com/hk/queenb/Constants.java
@@ -0,0 +1,20 @@
+package com.hk.queenb;
+
+import java.text.SimpleDateFormat;
+
+public class Constants {
+	public static final SimpleDateFormat formatDate = new SimpleDateFormat(
+			"E, dd.MMM.yyyy");
+
+	public static final SimpleDateFormat sqlFormat = new SimpleDateFormat(
+			"yyyy-MM-dd");
+
+	public static final String DATE = "date";
+	public static final String NOTE = "note";
+	public static final String EVENT_NEW = "new";
+	public static final String EVENT_DETAIL = "detail";
+	public static final String NO = "n";
+	public static final String YES = "y";
+	public static final int DIALOG_LENGTH = 30;
+
+}
---
+++ trunk/QueenB/src/com/hk/queenb/MainActivity.java
@@ -0,0 +1,45 @@
+package com.hk.queenb;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.google.analytics.tracking.android.EasyTracker;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.single_column_main);
+		// dualPane = findViewById(R.id.right_column) != null;
+		PreferenceManager.getDefaultSharedPreferences(this);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EasyTracker.getInstance().activityStart(this); // Add this method.
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this); // Add this method.
+	}
+
+	public void onVytvorNovyChov(View v) {
+		Intent i = new Intent(MainActivity.this, NewQueen.class);
+		startActivity(i);
+	}
+
+	public void onZoznamChovov(View v) {
+		Intent i = new Intent(MainActivity.this, QueensList.class);
+		startActivity(i);
+	}
+
+}
