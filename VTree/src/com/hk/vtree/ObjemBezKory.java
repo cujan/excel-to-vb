8d604112ea72ecd42e951dd89776d0f240f60dec
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2013-10-22 20:09:53 +0000

    Dopracovany vypocet a dodany spinner....

--- VTree/src/com/hk/vtree/ObjemBezKory.java
+++ VTree/src/com/hk/vtree/ObjemBezKory.java
@@ -1,10 +1,16 @@
 package com.hk.vtree;
 
+import java.security.PublicKey;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.*;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.*;
 
 public class ObjemBezKory extends Activity {
 
@@ -19,8 +25,22 @@ public class ObjemBezKory extends Activity {
 	public void onClickVypocitajObjemKmena(View v){
 		
 		TextView vysledok = (TextView)findViewById(R.id.textViewVysledok);
+		Spinner typDrevaSpinner = (Spinner)findViewById(R.id.spinnerTypDreva);
 		
-		vysledok.setText(getResult());
+		String typStromu = "";
+		
+		String vybranyTypStromu = String.valueOf(typDrevaSpinner.getSelectedItem());
+		
+		if (vybranyTypStromu == null || vybranyTypStromu.equals(String.valueOf(""))
+			|| vybranyTypStromu.isEmpty()) {
+		Toast.makeText(this, R.string.no_tree_type_selected,
+			Toast.LENGTH_SHORT).show();
+		return;
+		} else {
+		typStromu = Constants.KeyMap.get(vybranyTypStromu);
+		}
+		
+		vysledok.setText(getResult()+typStromu);
 		
 		
 	}
@@ -28,19 +48,38 @@ public class ObjemBezKory extends Activity {
 	public String getResult(){
 		EditText dlzkaKmena = (EditText)findViewById(R.id.editTextDlzkaKmena);
 		EditText sirkaKmena = (EditText)findViewById(R.id.editTextSirkaKmena);
-		String result;
-		
 		Double dlzka = Double.valueOf(dlzkaKmena.getText().toString());
 		Double sirka = Double.valueOf(sirkaKmena.getText().toString());
+		
+		String result;
+		
+		
+		
+		
 		Double objem;
+		Double hrubkaKory = koeficient();
 		
-		objem = dlzka*sirka;
+		objem = (Math.PI*dlzka*Math.pow((sirka - hrubkaKory),2))/40000;
+		//DecimalFormat f = new DecimalFormat("##.00");
+		//f.format(objem); 
+		double zaokruhlene = Math.round(objem*100)/100D;
 		
 		
-		
-		
-		result = objem.toString();
+		result = objem.toString()+ "--"	+zaokruhlene+"m3";
 		return result;
+	}
+	
+	public double koeficient(){
+		EditText sirkaKmena = (EditText)findViewById(R.id.editTextSirkaKmena);
+		Double sirka = Double.valueOf(sirkaKmena.getText().toString());
+		double hrubka = 0;
+		double p0 = 0.57723;
+		double p1 = 0.006897;
+		double p2 = 1.3123;
+		
+		hrubka =p0 + p1*Math.pow(sirka, p2);
+		
+		return hrubka;
 	}
 
 	@Override
