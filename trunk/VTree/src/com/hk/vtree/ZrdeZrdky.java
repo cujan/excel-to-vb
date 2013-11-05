dcbcc509c5ee089f2903d357f8c7594698c3a9d5
holubjan <holubjan@70332664-8a54-11dd-8937-b3a01dae0fbe>
2013-11-05 12:16:25 +0000

    zacata praca na funkcii listener pre EditText v zrde a zrdky...

--- trunk/VTree/src/com/hk/vtree/ZrdeZrdky.java
+++ trunk/VTree/src/com/hk/vtree/ZrdeZrdky.java
@@ -18,7 +18,34 @@ public class ZrdeZrdky extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_zrde_zrdky);
 		
+		EditText etPokus = (EditText)findViewById(R.id.editTextIhlZrdky1);
+		final TextView tvPokus = (TextView)findViewById(R.id.textViewIhlZrdky1);
 		
+		
+		TextWatcher cakacZmeny = new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				vypocty.tester();
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		
+		etPokus.addTextChangedListener(cakacZmeny);
 		
 		
 	}
@@ -35,7 +62,7 @@ public class ZrdeZrdky extends Activity {
 		
 		TextView objemik = (TextView) findViewById(R.id.textViewIhlZrde1);
 		objemik.setText(vypocty.tester());
-		Toast.makeText(this, "pokus - tester",Toast.LENGTH_LONG).show();
+		//Toast.makeText(this, "pokus - tester",Toast.LENGTH_LONG).show();
 		
 		
 		
